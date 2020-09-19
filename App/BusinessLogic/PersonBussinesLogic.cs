16498c1d5505bb5681f0daa82a186a06bd7043d7
vdamasco <>
2020-09-19 16:42:53 -0300

    add Person

--- App/BusinessLogic/PersonBussinesLogic.cs
+++ App/BusinessLogic/PersonBussinesLogic.cs
@@ -2,6 +2,25 @@ namespace BusinessLogic
 {
     public class PersonBussinesLogic
     {
-        
+        public void CreateUser(in string token)
+        {
+            
+        }
+        public void SetPassword(in int id, in string token)
+        {
+            
+        }
+        public void UpdateEmail(in int id, in string email, in string token)
+        {
+            
+        }
+        public void UpdatePassword(in int id, in string password, in string token)
+        {
+            
+        }
+        public void UpdateName(in int id, in string name, in string token)
+        {
+            
+        }
     }
 }
