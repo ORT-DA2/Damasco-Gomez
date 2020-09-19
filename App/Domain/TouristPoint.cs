c38f0f2852c9b22949a9124f855a412e802f97f6
yuligomez <yulianagomezsilva@gmail.com>
2020-09-19 14:25:25 -0300

    create class TouristPoint and update uml

--- App/Domain/TouristPoint.cs
+++ App/Domain/TouristPoint.cs
@@ -2,6 +2,27 @@ namespace Domain
 {
     public class TouristPoint
     {
-        
+         public int Id {get; set;}
+
+         public string Name {get; set;}
+
+         public Image Image {get; set; }
+
+        public string Description {get; set;}
+
+        public void UpdateImage ( Image image)
+        {
+            Image = image;
+
+        }
+        public void UpdateDescription (string description)
+        {
+
+            Description = description;
+        }
+        public UdpateName (string name)
+        {
+            Name = name ;
+        }
     }
 }
