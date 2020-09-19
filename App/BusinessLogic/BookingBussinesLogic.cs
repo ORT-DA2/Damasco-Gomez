e7f4fb624d8dd6127465f79dc8e126cf2dcebbd5
vdamasco <>
2020-09-19 16:21:30 -0300

    Add class Booking

---
+++ App/BusinessLogic/BookingBussinesLogic.cs
@@ -0,0 +1,31 @@
+namespace BusinessLogic
+{
+    public class BookingBussinesLogic
+    {
+        public void GetBookingById(in int id)
+        {
+
+        }
+
+        public void GetBookingByName(in string name)
+        {
+
+        }
+
+        public void CreateBooking(in int checkIn, in int checkOut, in string name, in string email, in string house, in string token)
+        {
+
+        }
+
+        public void GetCode(in int id, in string token)
+        {
+
+        }
+
+        public void DeleteBooking(in int id, in string token)
+        {
+
+        }
+
+    }
+}
