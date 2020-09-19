6185aada66e081f3b14c7870bcf92708a7b5441f
yuligomez <yulianagomezsilva@gmail.com>
2020-09-19 14:09:20 -0300

    create House

---
+++ App/Domain/House.cs
@@ -0,0 +1,43 @@
+namespace Domain
+{
+    public class House
+    {
+        
+        public int Id {get ; set ; }
+
+        public bool Avaible {get ; set; }
+       
+        public int PricePerNight {get; set;}
+        public Spot TouristPoint {get ; set; }
+        
+        public string HouseName {get ; set; }
+        
+       public int Starts {get ; set; }
+       
+       public string Address {get ; set; }
+
+       public List<Image> Image {get ; set; }
+
+       public string Description {get ; set;}
+
+       public int Phone {get; set; }
+
+       public string Contanct {get; set;}
+
+       
+
+      
+        public void udpatePricePerNight (int id) 
+        {
+            PricePerNight = id;
+        }
+
+        public void udpateContactInformation (int id , string phone , string contact)
+        {
+             Id = id;
+             Phone = phone;
+             Contact = contact;
+        }
+
+    } 
+}
