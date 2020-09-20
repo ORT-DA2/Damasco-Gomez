de1bede07433c9eca7d86b7a05e3436b9166482d
vdamasco <>
2020-09-20 14:19:18 -0300

    Add gitignore for bin adn obj

--- App/Domain.Test/TouristPointTest.cs
+++ App/Domain.Test/TouristPointTest.cs
@@ -5,34 +5,5 @@ namespace Domain.Test
     [TestClass]
     public class TouristPointTest
     {
-        [TestMethod]
-        public void TestUdpateImge()
-        {
-            Image newImage = new Image ();
-            TouristPoint  touristPoint = new TouristPoint();
-            touristPoint.UpdateImage(newImage);
-            Image realImage = touristPoint.Image();
-            assertEquals(realImage, newImage);
-        }
-        [TestMethod]
-        public void TestUdpateDescription()
-        {
-            string newDescription = "Un lugar paridisíaco";
-            TouristPoint  touristPoint = new TouristPoint();
-            touristPoint.UpdateDescription(newDescription);
-            string realDescription = touristPoint.Description();
-            assertEquals(realDescription , newDescription);
-
-
-        }
-          [TestMethod]
-        public void TestUdpateName()
-        {
-            string newName = "Yuliana";
-            TouristPoint touristPoint = new TouristPoint();
-            touristPoint.UdpateName(newName);
-            string realName = touristPoint.Name();
-            assertEquals(realName , newName);
-        }
     }
 }
