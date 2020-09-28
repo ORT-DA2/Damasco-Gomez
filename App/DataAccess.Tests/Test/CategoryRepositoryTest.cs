e55569bb721bbd15b5501b911c8af44ae6712c6b
vdamasco <>
2020-09-28 17:18:03 -0300

    Edit methods to test , to have all category

--- App/DataAccess.Tests/Test/CategoryRepositoryTest.cs
+++ App/DataAccess.Tests/Test/CategoryRepositoryTest.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using DataAccess.Context;
@@ -52,12 +53,57 @@ namespace DataAccess.Tests.Test
             mockDbContext = new Mock<DbContext>(MockBehavior.Strict);
         }
         [TestMethod]
+        public void TestAdd()
+        {
+            Category category = new Category(){Id = 123, Name="name new"};
+            mockDbContext.Setup(d => d.Set<Category>()).Returns(mockSet.GetMockDbSet(categoriesToReturn).Object);
+            int categories = categoriesToReturn.Count();
+            mockDbContext.Setup(d => d.SaveChanges()).Returns(category.Id);
+            repositoryMaster = new RepositoryMaster(mockDbContext.Object);
+            repository = new CategoryRepository(repositoryMaster);
+
+            repository.Add(category);
+
+            Assert.AreEqual(categories+1, categoriesToReturn.Count());
+        }
+        [TestMethod]
+        public void TestAddFailValidate()
+        {
+            Category category = new Category(){Id = 123, Name="name new"};
+            int categoryLenght = categoriesToReturn.Count() ;
+            mockDbContext.Setup(d => d.Set<Category>()).Returns(mockSet.GetMockDbSet(categoriesToReturn).Object);
+            mockDbContext.Setup(d => d.SaveChanges()).Returns(category.Id);
+            repositoryMaster = new RepositoryMaster(mockDbContext.Object);
+            repository = new CategoryRepository(repositoryMaster);
+
+            repository.Add(category);
+
+            //Assert.AreEqual(categoryLenght,repositoryMaster.Categorys.Count() + 1);
+        }
+        [TestMethod]
+        public void TestAddFailExist()
+        {
+            Category category = categoriesToReturn.First();
+            ArgumentException exception = new ArgumentException();
+            var _mockSet = mockSet.GetMockDbSet(categoriesToReturn);
+            _mockSet.Setup(d => d.Find(It.IsAny<Category>())).Throws(exception);
+            mockDbContext.Setup(d => d.Set<Category>()).Returns(_mockSet.Object);
+            repositoryMaster = new RepositoryMaster(mockDbContext.Object);
+            repository = new CategoryRepository(repositoryMaster);
+
+            //repository.Add(category);
+
+            //Assert.AreEqual();
+        }
+        [TestMethod]
         public void TestGetAllCategorysOk()
         {
             mockDbContext.Setup(d => d.Set<Category>()).Returns(mockSet.GetMockDbSet(categoriesToReturn).Object);
             repositoryMaster = new RepositoryMaster(mockDbContext.Object);
             repository = new CategoryRepository(repositoryMaster);
+
             var result = repository.GetElements();
+
             Assert.IsTrue(categoriesToReturn.SequenceEqual(result));
         }
         [TestMethod]
@@ -67,28 +113,34 @@ namespace DataAccess.Tests.Test
             mockDbContext.Setup(d => d.Set<Category>()).Returns(mockSet.GetMockDbSet(emptyCategory).Object);
             repositoryMaster = new RepositoryMaster(mockDbContext.Object);
             repository = new CategoryRepository(repositoryMaster);
+
             var result = repository.GetElements();
+
             Assert.IsTrue(emptyCategory.SequenceEqual(result));
         }
         [TestMethod]
-        public void TestExistNot()
-        {
-            Category category = categoriesToReturn.First();
-            mockDbContext.Setup(d => d.Set<Category>()).Returns(mockSet.GetMockDbSet(emptyCategory).Object);
-            repositoryMaster = new RepositoryMaster(mockDbContext.Object);
-            repository = new CategoryRepository(repositoryMaster);
-            bool result = repository.ExistElement(category);
-            Assert.IsFalse(result);
-        }
-        [TestMethod]
-        public void TestExist()
+        public void TestExistElement()
         {
             Category category = categoriesToReturn.First();
             mockDbContext.Setup(d => d.Set<Category>()).Returns(mockSet.GetMockDbSet(categoriesToReturn).Object);
             repositoryMaster = new RepositoryMaster(mockDbContext.Object);
             repository = new CategoryRepository(repositoryMaster);
+
             bool result = repository.ExistElement(category);
+
             Assert.IsTrue(result);
+        }
+        [TestMethod]
+        public void TestExistElementFail()
+        {
+            Category category = categoriesToReturn.First();
+            mockDbContext.Setup(d => d.Set<Category>()).Returns(mockSet.GetMockDbSet(emptyCategory).Object);
+            repositoryMaster = new RepositoryMaster(mockDbContext.Object);
+            repository = new CategoryRepository(repositoryMaster);
+
+            bool result = repository.ExistElement(category);
+
+            Assert.IsFalse(result);
         }
 
         [TestMethod]
@@ -98,50 +150,66 @@ namespace DataAccess.Tests.Test
             mockDbContext.Setup(d => d.Set<Category>()).Returns(mockSet.GetMockDbSet(categoriesToReturn).Object);
             repositoryMaster = new RepositoryMaster(mockDbContext.Object);
             repository = new CategoryRepository(repositoryMaster);
+
             bool result = repository.ExistElement(categoryId);
+
             Assert.IsFalse(result);
         }
-        // [TestMethod]
-        // public void TestExistCategoryById()
-        // {
-        //     Category category = categoriesToReturn.First();
-        //     mockDbContext.Setup(d => d.Set<Category>()).Returns(mockSet.GetMockDbSet(categoriesToReturn).Object);
-        //     repositoryMaster = new RepositoryMaster(mockDbContext.Object);
-        //     repository = new CategoryRepository(repositoryMaster);
-        //     bool result = repository.ExistElement(category.Id);
-        //     Assert.IsTrue(result);
-        // }
         [TestMethod]
-        public void TestExistCategory()
+        public void TestExistById()
         {
             Category category = categoriesToReturn.First();
+            var _mockSet = mockSet.GetMockDbSet(categoriesToReturn);
+            _mockSet.Setup(d => d.Find(It.IsAny<object[]>())).Returns(category);
+            mockDbContext.Setup(d => d.Set<Category>()).Returns(_mockSet.Object);
+            repositoryMaster = new RepositoryMaster(mockDbContext.Object);
+            repository = new CategoryRepository(repositoryMaster);
+
+            bool result = repository.ExistElement(category.Id);
+
+            Assert.IsTrue(result);
+        }
+        [TestMethod]
+        public void TestExistByIdFail()
+        {
+            Category category = new Category(){Id=123423};
             mockDbContext.Setup(d => d.Set<Category>()).Returns(mockSet.GetMockDbSet(categoriesToReturn).Object);
             repositoryMaster = new RepositoryMaster(mockDbContext.Object);
             repository = new CategoryRepository(repositoryMaster);
-            bool result = repository.ExistElement(category);
-            Assert.IsTrue(result);
+
+            bool result = repository.ExistElement(category.Id);
+
+            Assert.IsFalse(result);
         }
-        // [TestMethod]
-        // public void TestFind()
-        // {
-        //     Category category = categoriesToReturn.First();
-        //     mockDbContext.Setup(d => d.Set<Category>()).Returns(mockSet.GetMockDbSet(categoriesToReturn).Object);
-        //     repositoryMaster = new RepositoryMaster(mockDbContext.Object);
-        //     repository = new CategoryRepository(repositoryMaster);
-        //     Category result = repository.Find(category.Id);
-        //     Assert.AreEqual(result,category);
-        // }
+        [TestMethod]
+        public void TestFind()
+        {
+            Category category = categoriesToReturn.First();
+            var _mockSet = mockSet.GetMockDbSet(categoriesToReturn);
+            _mockSet.Setup(d => d.Find(It.IsAny<object[]>())).Returns(category);
+            mockDbContext.Setup(d => d.Set<Category>()).Returns(_mockSet.Object);
+            repositoryMaster = new RepositoryMaster(mockDbContext.Object);
+            repository = new CategoryRepository(repositoryMaster);
+
+            Category result = repository.Find(category.Id);
+
+            Assert.AreEqual(result,category);
+        }
 
         [TestMethod]
         public void TestFindFail()
         {
-            Category category = categoriesToReturn.First();
-            mockDbContext.Setup(d => d.Set<Category>()).Returns(mockSet.GetMockDbSet(categoriesToReturn).Object);
+            Category category = new Category(){Id=232323};
+            Category categoryNull = null;
+            var _mockSet = mockSet.GetMockDbSet(categoriesToReturn);
+            ArgumentException exception = new ArgumentException();
+            _mockSet.Setup(d => d.Find(It.IsAny<object[]>())).Returns(categoryNull);
+            mockDbContext.Setup(d => d.Set<Category>()).Returns(_mockSet.Object);
             repositoryMaster = new RepositoryMaster(mockDbContext.Object);
             repository = new CategoryRepository(repositoryMaster);
-            Category result = repository.Find(category.Id + 1000);
-            // Exception exception = new ArgumentException();
-            // Assert.IsInstanceOfType(result, typeof(Exception));
+
+            Category result = repository.Find(category.Id);
+
             Assert.IsNull(result);
         }
         [TestMethod]
@@ -151,10 +219,12 @@ namespace DataAccess.Tests.Test
             category.Name = "New name of category";
             string newName = category.Name;
             mockDbContext.Setup(d => d.Set<Category>()).Returns(mockSet.GetMockDbSet(categoriesToReturn).Object);
-            mockDbContext.Setup(d => d.SaveChanges()).Returns(categoriesToReturn.First().Id);
+            mockDbContext.Setup(d => d.SaveChanges()).Returns(category.Id);
             repositoryMaster = new RepositoryMaster(mockDbContext.Object);
             repository = new CategoryRepository(repositoryMaster);
+
             repository.Update(category);
+
             Assert.AreEqual(category.Name,newName);
         }
         [TestMethod]
@@ -162,13 +232,78 @@ namespace DataAccess.Tests.Test
         {
             Category category = new Category(){Id = 13000};
             string newName = category.Name;
+            // Exception exception = new ArgumentException();
             mockDbContext.Setup(d => d.Set<Category>()).Returns(mockSet.GetMockDbSet(categoriesToReturn).Object);
             mockDbContext.Setup(d => d.SaveChanges()).Returns(categoriesToReturn.First().Id);
             repositoryMaster = new RepositoryMaster(mockDbContext.Object);
             repository = new CategoryRepository(repositoryMaster);
+
             repository.Update(category);
-            // Exception exception = new ArgumentException();
+
             // Assert.IsInstanceOfType(result, typeof(Exception));
+        }
+        [TestMethod]
+        public void TestDelete()
+        {
+            Category category = categoriesToReturn.First();
+            int lengthCategorys = categoriesToReturn.Count();
+            mockDbContext.Setup(d => d.Set<Category>()).Returns(mockSet.GetMockDbSet(categoriesToReturn).Object);
+            mockDbContext.Setup(d => d.SaveChanges()).Returns(category.Id);
+            mockDbContext.Setup(d => d.Remove(category));
+            repositoryMaster = new RepositoryMaster(mockDbContext.Object);
+            repository = new CategoryRepository(repositoryMaster);
+
+            repository.Delete(category);
+
+            //Assert.AreEqual(categoriesToReturn.Count, lengthCategorys - 1 );
+        }
+        [TestMethod]
+        public void TestDeleteFailExist()
+        {
+            Category category = categoriesToReturn.First();
+            int lengthCategorys = categoriesToReturn.Count();
+            mockDbContext.Setup(d => d.Set<Category>()).Returns(mockSet.GetMockDbSet(categoriesToReturn).Object);
+            mockDbContext.Setup(d => d.SaveChanges()).Returns(category.Id);
+            repositoryMaster = new RepositoryMaster(mockDbContext.Object);
+            repository = new CategoryRepository(repositoryMaster);
+
+            repository.Delete(category);
+
+            //Assert.AreEqual(categoriesToReturn.Count, lengthCategorys - 1 );
+        }
+        [TestMethod]
+        public void TestDeleteById()
+        {
+            Category category = categoriesToReturn.First();
+            int lengthCategorys = categoriesToReturn.Count();
+            var _mockSet = mockSet.GetMockDbSet(categoriesToReturn);
+            _mockSet.Setup(d => d.Find(It.IsAny<object[]>())).Returns(category);
+            mockDbContext.Setup(d => d.Set<Category>()).Returns(_mockSet.Object);
+            mockDbContext.Setup(d => d.SaveChanges()).Returns(category.Id);
+            //mockDbContext.Setup(d => d.Remove(category.Id));
+            repositoryMaster = new RepositoryMaster(mockDbContext.Object);
+            repository = new CategoryRepository(repositoryMaster);
+
+            repository.Delete(category.Id);
+
+            //Assert.AreEqual(categoriesToReturn.Count, lengthCategorys - 1 );
+        }
+        [TestMethod]
+        public void TestDeleteByIdFailExist()
+        {
+            Category category = categoriesToReturn.First();
+            Category categoryNull = null;
+            int lengthCategorys = categoriesToReturn.Count();
+            var _mockSet = mockSet.GetMockDbSet(categoriesToReturn);
+            _mockSet.Setup(d => d.Find(It.IsAny<object[]>())).Returns(categoryNull);
+            mockDbContext.Setup(d => d.Set<Category>()).Returns(_mockSet.Object);
+            mockDbContext.Setup(d => d.SaveChanges()).Returns(category.Id);
+            repositoryMaster = new RepositoryMaster(mockDbContext.Object);
+            repository = new CategoryRepository(repositoryMaster);
+
+            //repository.Delete(category.Id);
+
+            //Assert.AreEqual(categoriesToReturn.Count, lengthCategorys - 1 );
         }
     }
 }
