a841ec0a1a55579f8c9611ea2ff17f1eba138deb
vdamasco <>
2020-09-28 17:22:00 -0300

    Person testing to have all

--- App/DataAccess.Tests/Test/PersonRepositoryTest.cs
+++ App/DataAccess.Tests/Test/PersonRepositoryTest.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using DataAccess.Context;
@@ -52,12 +53,57 @@ namespace DataAccess.Tests.Test
             mockDbContext = new Mock<DbContext>(MockBehavior.Strict);
         }
         [TestMethod]
+        public void TestAdd()
+        {
+            Person person = new Person(){Id = 123, Email="name new"};
+            mockDbContext.Setup(d => d.Set<Person>()).Returns(mockSet.GetMockDbSet(personsToReturn).Object);
+            int persons = personsToReturn.Count();
+            mockDbContext.Setup(d => d.SaveChanges()).Returns(person.Id);
+            repositoryMaster = new RepositoryMaster(mockDbContext.Object);
+            repository = new PersonRepository(repositoryMaster);
+
+            repository.Add(person);
+
+            Assert.AreEqual(persons+1, personsToReturn.Count());
+        }
+        [TestMethod]
+        public void TestAddFailValidate()
+        {
+            Person person = new Person(){Id = 123, Email="name new"};
+            int personLenght = personsToReturn.Count() ;
+            mockDbContext.Setup(d => d.Set<Person>()).Returns(mockSet.GetMockDbSet(personsToReturn).Object);
+            mockDbContext.Setup(d => d.SaveChanges()).Returns(person.Id);
+            repositoryMaster = new RepositoryMaster(mockDbContext.Object);
+            repository = new PersonRepository(repositoryMaster);
+
+            repository.Add(person);
+
+            //Assert.AreEqual(personLenght,repositoryMaster.Persons.Count() + 1);
+        }
+        [TestMethod]
+        public void TestAddFailExist()
+        {
+            Person person = personsToReturn.First();
+            ArgumentException exception = new ArgumentException();
+            var _mockSet = mockSet.GetMockDbSet(personsToReturn);
+            _mockSet.Setup(d => d.Find(It.IsAny<Person>())).Throws(exception);
+            mockDbContext.Setup(d => d.Set<Person>()).Returns(_mockSet.Object);
+            repositoryMaster = new RepositoryMaster(mockDbContext.Object);
+            repository = new PersonRepository(repositoryMaster);
+
+            //repository.Add(person);
+
+            //Assert.AreEqual();
+        }
+        [TestMethod]
         public void TestGetAllPersonsOk()
         {
             mockDbContext.Setup(d => d.Set<Person>()).Returns(mockSet.GetMockDbSet(personsToReturn).Object);
             repositoryMaster = new RepositoryMaster(mockDbContext.Object);
             repository = new PersonRepository(repositoryMaster);
+
             var result = repository.GetElements();
+
             Assert.IsTrue(personsToReturn.SequenceEqual(result));
         }
         [TestMethod]
@@ -67,28 +113,34 @@ namespace DataAccess.Tests.Test
             mockDbContext.Setup(d => d.Set<Person>()).Returns(mockSet.GetMockDbSet(emptyPerson).Object);
             repositoryMaster = new RepositoryMaster(mockDbContext.Object);
             repository = new PersonRepository(repositoryMaster);
+
             var result = repository.GetElements();
+
             Assert.IsTrue(emptyPerson.SequenceEqual(result));
         }
         [TestMethod]
-        public void TestExistNot()
-        {
-            Person person = personsToReturn.First();
-            mockDbContext.Setup(d => d.Set<Person>()).Returns(mockSet.GetMockDbSet(emptyPerson).Object);
-            repositoryMaster = new RepositoryMaster(mockDbContext.Object);
-            repository = new PersonRepository(repositoryMaster);
-            bool result = repository.ExistElement(person);
-            Assert.IsFalse(result);
-        }
-        [TestMethod]
-        public void TestExist()
+        public void TestExistElement()
         {
             Person person = personsToReturn.First();
             mockDbContext.Setup(d => d.Set<Person>()).Returns(mockSet.GetMockDbSet(personsToReturn).Object);
             repositoryMaster = new RepositoryMaster(mockDbContext.Object);
             repository = new PersonRepository(repositoryMaster);
+
             bool result = repository.ExistElement(person);
+
             Assert.IsTrue(result);
+        }
+        [TestMethod]
+        public void TestExistElementFail()
+        {
+            Person person = personsToReturn.First();
+            mockDbContext.Setup(d => d.Set<Person>()).Returns(mockSet.GetMockDbSet(emptyPerson).Object);
+            repositoryMaster = new RepositoryMaster(mockDbContext.Object);
+            repository = new PersonRepository(repositoryMaster);
+
+            bool result = repository.ExistElement(person);
+
+            Assert.IsFalse(result);
         }
 
         [TestMethod]
@@ -98,50 +150,66 @@ namespace DataAccess.Tests.Test
             mockDbContext.Setup(d => d.Set<Person>()).Returns(mockSet.GetMockDbSet(personsToReturn).Object);
             repositoryMaster = new RepositoryMaster(mockDbContext.Object);
             repository = new PersonRepository(repositoryMaster);
+
             bool result = repository.ExistElement(personId);
+
             Assert.IsFalse(result);
         }
-        // [TestMethod]
-        // public void TestExistPersonById()
-        // {
-        //     Person person = personsToReturn.First();
-        //     mockDbContext.Setup(d => d.Set<Person>()).Returns(mockSet.GetMockDbSet(personsToReturn).Object);
-        //     repositoryMaster = new RepositoryMaster(mockDbContext.Object);
-        //     repository = new PersonRepository(repositoryMaster);
-        //     bool result = repository.ExistElement(person.Id);
-        //     Assert.IsTrue(result);
-        // }
         [TestMethod]
-        public void TestExistPerson()
+        public void TestExistById()
         {
             Person person = personsToReturn.First();
+            var _mockSet = mockSet.GetMockDbSet(personsToReturn);
+            _mockSet.Setup(d => d.Find(It.IsAny<object[]>())).Returns(person);
+            mockDbContext.Setup(d => d.Set<Person>()).Returns(_mockSet.Object);
+            repositoryMaster = new RepositoryMaster(mockDbContext.Object);
+            repository = new PersonRepository(repositoryMaster);
+
+            bool result = repository.ExistElement(person.Id);
+
+            Assert.IsTrue(result);
+        }
+        [TestMethod]
+        public void TestExistByIdFail()
+        {
+            Person person = new Person(){Id=123423};
             mockDbContext.Setup(d => d.Set<Person>()).Returns(mockSet.GetMockDbSet(personsToReturn).Object);
             repositoryMaster = new RepositoryMaster(mockDbContext.Object);
             repository = new PersonRepository(repositoryMaster);
-            bool result = repository.ExistElement(person);
-            Assert.IsTrue(result);
+
+            bool result = repository.ExistElement(person.Id);
+
+            Assert.IsFalse(result);
         }
-        // [TestMethod]
-        // public void TestFind()
-        // {
-        //     Person person = personsToReturn.First();
-        //     mockDbContext.Setup(d => d.Set<Person>()).Returns(mockSet.GetMockDbSet(personsToReturn).Object);
-        //     repositoryMaster = new RepositoryMaster(mockDbContext.Object);
-        //     repository = new PersonRepository(repositoryMaster);
-        //     Person result = repository.Find(person.Id);
-        //     Assert.AreEqual(result,person);
-        // }
+        [TestMethod]
+        public void TestFind()
+        {
+            Person person = personsToReturn.First();
+            var _mockSet = mockSet.GetMockDbSet(personsToReturn);
+            _mockSet.Setup(d => d.Find(It.IsAny<object[]>())).Returns(person);
+            mockDbContext.Setup(d => d.Set<Person>()).Returns(_mockSet.Object);
+            repositoryMaster = new RepositoryMaster(mockDbContext.Object);
+            repository = new PersonRepository(repositoryMaster);
+
+            Person result = repository.Find(person.Id);
+
+            Assert.AreEqual(result,person);
+        }
 
         [TestMethod]
         public void TestFindFail()
         {
-            Person person = personsToReturn.First();
-            mockDbContext.Setup(d => d.Set<Person>()).Returns(mockSet.GetMockDbSet(personsToReturn).Object);
+            Person person = new Person(){Id=232323};
+            Person personNull = null;
+            var _mockSet = mockSet.GetMockDbSet(personsToReturn);
+            ArgumentException exception = new ArgumentException();
+            _mockSet.Setup(d => d.Find(It.IsAny<object[]>())).Returns(personNull);
+            mockDbContext.Setup(d => d.Set<Person>()).Returns(_mockSet.Object);
             repositoryMaster = new RepositoryMaster(mockDbContext.Object);
             repository = new PersonRepository(repositoryMaster);
-            Person result = repository.Find(person.Id + 1000);
-            // Exception exception = new ArgumentException();
-            // Assert.IsInstanceOfType(result, typeof(Exception));
+
+            Person result = repository.Find(person.Id);
+
             Assert.IsNull(result);
         }
         [TestMethod]
@@ -151,10 +219,12 @@ namespace DataAccess.Tests.Test
             person.Email = "New name of person";
             string newEmail = person.Email;
             mockDbContext.Setup(d => d.Set<Person>()).Returns(mockSet.GetMockDbSet(personsToReturn).Object);
-            mockDbContext.Setup(d => d.SaveChanges()).Returns(personsToReturn.First().Id);
+            mockDbContext.Setup(d => d.SaveChanges()).Returns(person.Id);
             repositoryMaster = new RepositoryMaster(mockDbContext.Object);
             repository = new PersonRepository(repositoryMaster);
+
             repository.Update(person);
+
             Assert.AreEqual(person.Email,newEmail);
         }
         [TestMethod]
@@ -162,13 +232,78 @@ namespace DataAccess.Tests.Test
         {
             Person person = new Person(){Id = 13000};
             string newEmail = person.Email;
+            // Exception exception = new ArgumentException();
             mockDbContext.Setup(d => d.Set<Person>()).Returns(mockSet.GetMockDbSet(personsToReturn).Object);
             mockDbContext.Setup(d => d.SaveChanges()).Returns(personsToReturn.First().Id);
             repositoryMaster = new RepositoryMaster(mockDbContext.Object);
             repository = new PersonRepository(repositoryMaster);
+
             repository.Update(person);
-            // Exception exception = new ArgumentException();
+
             // Assert.IsInstanceOfType(result, typeof(Exception));
+        }
+        [TestMethod]
+        public void TestDelete()
+        {
+            Person person = personsToReturn.First();
+            int lengthPersons = personsToReturn.Count();
+            mockDbContext.Setup(d => d.Set<Person>()).Returns(mockSet.GetMockDbSet(personsToReturn).Object);
+            mockDbContext.Setup(d => d.SaveChanges()).Returns(person.Id);
+            mockDbContext.Setup(d => d.Remove(person));
+            repositoryMaster = new RepositoryMaster(mockDbContext.Object);
+            repository = new PersonRepository(repositoryMaster);
+
+            repository.Delete(person);
+
+            //Assert.AreEqual(personsToReturn.Count, lengthPersons - 1 );
+        }
+        [TestMethod]
+        public void TestDeleteFailExist()
+        {
+            Person person = personsToReturn.First();
+            int lengthPersons = personsToReturn.Count();
+            mockDbContext.Setup(d => d.Set<Person>()).Returns(mockSet.GetMockDbSet(personsToReturn).Object);
+            mockDbContext.Setup(d => d.SaveChanges()).Returns(person.Id);
+            repositoryMaster = new RepositoryMaster(mockDbContext.Object);
+            repository = new PersonRepository(repositoryMaster);
+
+            repository.Delete(person);
+
+            //Assert.AreEqual(personsToReturn.Count, lengthPersons - 1 );
+        }
+        [TestMethod]
+        public void TestDeleteById()
+        {
+            Person person = personsToReturn.First();
+            int lengthPersons = personsToReturn.Count();
+            var _mockSet = mockSet.GetMockDbSet(personsToReturn);
+            _mockSet.Setup(d => d.Find(It.IsAny<object[]>())).Returns(person);
+            mockDbContext.Setup(d => d.Set<Person>()).Returns(_mockSet.Object);
+            mockDbContext.Setup(d => d.SaveChanges()).Returns(person.Id);
+            //mockDbContext.Setup(d => d.Remove(person.Id));
+            repositoryMaster = new RepositoryMaster(mockDbContext.Object);
+            repository = new PersonRepository(repositoryMaster);
+
+            repository.Delete(person.Id);
+
+            //Assert.AreEqual(personsToReturn.Count, lengthPersons - 1 );
+        }
+        [TestMethod]
+        public void TestDeleteByIdFailExist()
+        {
+            Person person = personsToReturn.First();
+            Person personNull = null;
+            int lengthPersons = personsToReturn.Count();
+            var _mockSet = mockSet.GetMockDbSet(personsToReturn);
+            _mockSet.Setup(d => d.Find(It.IsAny<object[]>())).Returns(personNull);
+            mockDbContext.Setup(d => d.Set<Person>()).Returns(_mockSet.Object);
+            mockDbContext.Setup(d => d.SaveChanges()).Returns(person.Id);
+            repositoryMaster = new RepositoryMaster(mockDbContext.Object);
+            repository = new PersonRepository(repositoryMaster);
+
+            //repository.Delete(person.Id);
+
+            //Assert.AreEqual(personsToReturn.Count, lengthPersons - 1 );
         }
     }
 }
