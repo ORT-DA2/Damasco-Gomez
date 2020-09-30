0e8ef18464d785f5532958fed69929cb7a100a05
vdamasco <>
2020-09-30 19:33:18 -0300

    Remove unnecessary exports

--- App/DataAccess/Repositories/BookingRepository.cs
+++ App/DataAccess/Repositories/BookingRepository.cs
@@ -1,8 +1,6 @@
 using System;
-using System.Collections.Generic;
 using DataAccessInterface.Repositories;
 using Domain;
-using Microsoft.EntityFrameworkCore;
 
 namespace DataAccess.Repositories
 {
