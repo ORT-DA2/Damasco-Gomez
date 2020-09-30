7c70f76a07e61fc9afb3d35322cf198e1a87fc71
vdamasco <>
2020-09-30 18:47:37 -0300

    Add validation with test include

--- App/DataAccess/Repositories/BookingRepository.cs
+++ App/DataAccess/Repositories/BookingRepository.cs
@@ -15,7 +15,21 @@ namespace DataAccess.Repositories
 
         protected override void Validate(Booking element)
         {
-            //throw new NotImplementedException();
+            bool houseAvailable = false;
+            if (element.House != null)
+            {
+                houseAvailable = element.House.Avaible;
+            }
+            if (!houseAvailable)
+            {
+                throw new ArgumentException("House is not available");
+            }
+            bool checkInAndOut = !element.CheckIn.Equals(DateTime.MinValue)
+                && !element.CheckOut.Equals(DateTime.MinValue);
+            if (!checkInAndOut)
+            {
+                throw new ArgumentException("There's no dates to create the booking");
+            }
         }
     }
 }
