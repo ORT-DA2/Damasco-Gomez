4130b36d3dd5798e6bb29c62c17edf6fd559b08c
yuligomez <yulianagomezsilva@gmail.com>
2020-10-04 16:46:07 -0300

    create TouristPointBasicInfoModel

---
+++ App/Model/Out/TouristPointBasicInfoModel.cs
@@ -0,0 +1,27 @@
+using Domain;
+
+namespace Model.Out
+{
+    public class TouristPointBasicInfoModel
+    {
+          public int Id {get; set;}
+
+         public string Name {get; set;}
+
+         public string Image {get; set;}
+
+        public string Description {get; set;}
+
+        public int RegionId {get; set;}
+
+        public TouristPointBasicInfoModel (TouristPoint touristPoint)
+        {
+            this.Id = touristPoint.Id;
+            this.Name = touristPoint.Name;
+            this.Image = touristPoint.Image;
+            this.Description = touristPoint.Description;
+            this.RegionId = touristPoint.RegionId;
+
+        }
+    }
+}
