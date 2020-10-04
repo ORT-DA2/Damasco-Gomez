deafa7b2758ce7c4f46ed064f5c83c6e753eba2e
vdamasco <>
2020-10-04 11:53:08 -0300

    Refactor coverage webapi

--- App/WebApi/Startup.cs
+++ App/WebApi/Startup.cs
@@ -13,9 +13,11 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using System.Diagnostics.CodeAnalysis;
 
 namespace WebApi
 {
+    [ExcludeFromCodeCoverage]
     public class Startup
     {
         public Startup(IConfiguration configuration)
