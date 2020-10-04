a54a70b48f759d641977708fc32cb645ff9d44fd
yuligomez <yulianagomezsilva@gmail.com>
2020-10-04 17:51:28 -0300

    CategoryModel In

--- App/Model/In/CategoryModel.cs
+++ App/Model/In/CategoryModel.cs
@@ -1,7 +1,24 @@
+using System.Collections.Generic;
+using System.Linq;
+using Domain;
+using Domain.Entities;
+
 namespace Model.In
 {
     public class CategoryModel
     {
-        
+        public string Name {get; set;}
+        public  IEnumerable<int> TouristPoints {get; set;}
+        public Category ToEntity()
+        {
+            return new Category()
+            {
+                Name= this.Name,
+                CategoryTouristPoints = this.TouristPoints.Select(m => new CategoryTouristPoint()
+                {
+                    TouristPointId = m
+                }).ToList()
+            };
+        }
     }
 }
