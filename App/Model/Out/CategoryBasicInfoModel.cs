276de7a5a74295205b00d9cd51b1c8598953fcaa
yuligomez <yulianagomezsilva@gmail.com>
2020-10-04 18:02:22 -0300

    add CategoryBasicInfoModel

---
+++ App/Model/Out/CategoryBasicInfoModel.cs
@@ -0,0 +1,17 @@
+using Domain;
+
+namespace Model.Out
+{
+    public class CategoryBasicInfoModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public CategoryBasicInfoModel(Category category)
+         {
+            this.Id = category.Id;
+            this.Name = category.Name;
+        }
+
+     }
+ }
+ 
