c8e20e794c6f793ee1a99ad3f17272f4753a513f
vdamasco <>
2020-10-06 18:31:37 -0300

    Add basic and detail model to booking controller

--- App/Model/Out/BookingBasicModel.cs
+++ App/Model/Out/BookingBasicModel.cs
@@ -14,5 +14,17 @@ namespace Model
         public int Price {get; set;}
         public  DateTime CheckIn {get; set;}
         public DateTime CheckOut {get; set;}
+        public BookingBasicModel(Booking booking)
+        {
+            this.Id = booking.Id;
+            this.Name = booking.Name;
+            this.Email = booking.Email;
+            this.Code = booking.Code;
+            this.HouseId = booking.HouseId;
+            this.State = booking.State;
+            this.Price = booking.Price;
+            this.CheckIn = booking.CheckIn;
+            this.CheckOut = booking.CheckOut;
+        }
     }
 }
--- App/Model/Out/BookingDetailModel.cs
+++ App/Model/Out/BookingDetailModel.cs
@@ -15,5 +15,18 @@ namespace Model
         public int Price {get; set;}
         public  DateTime CheckIn {get; set;}
         public DateTime CheckOut {get; set;}
+        public BookingDetailModel(Booking booking)
+        {
+            this.Id = booking.Id;
+            this.Name = booking.Name;
+            this.Email = booking.Email;
+            this.Code = booking.Code;
+            this.HouseId = booking.HouseId;
+            this.House = booking.House;
+            this.State = booking.State;
+            this.Price = booking.Price;
+            this.CheckIn = booking.CheckIn;
+            this.CheckOut = booking.CheckOut;
+        }
     }
 }
