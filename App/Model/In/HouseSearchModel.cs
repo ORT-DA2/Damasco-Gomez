bd400416dd2ad0f3ced3d08ac753589deb804639
yuligomez <yulianagomezsilva@gmail.com>
2020-10-06 19:15:54 -0300

    create HouseSearchModel IN

---
+++ App/Model/In/HouseSearchModel.cs
@@ -0,0 +1,15 @@
+using Domain;
+
+namespace Model.In
+{
+    public class HouseSearchModel
+    {
+        public string CheckIn {get; private set;}
+        public string CheckOut{get; private set;}
+         public TouristPoint TouristPoint {get ; private set; }
+         public int CantAdults {get; private set;}
+         public int CantChildrens {get; private set;}
+         public int CantBabys {get; private set;}
+
+    }
+}
