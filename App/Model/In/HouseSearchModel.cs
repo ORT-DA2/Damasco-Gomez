699fabb940f41845f5097e2044031bdb7b173001
yuligomez <yulianagomezsilva@gmail.com>
2020-10-06 22:09:07 -0300

    HouseSearch ToEntity

--- App/Model/In/HouseSearchModel.cs
+++ App/Model/In/HouseSearchModel.cs
@@ -1,4 +1,5 @@
 using Domain;
+using Domain.Entities;
 
 namespace Model.In
 {
@@ -10,6 +11,19 @@ namespace Model.In
          public int CantAdults {get; private set;}
          public int CantChildrens {get; private set;}
          public int CantBabys {get; private set;}
+
+          public HouseSearch ToEntity()
+        {
+            return new HouseSearch()
+            {
+                CheckIn = this.CheckIn,
+                CheckOut = this.CheckOut,
+                TouristPointId = this.TouristPointId,
+                CantAdults = this.CantAdults,
+                CantChildrens = this.CantChildrens,
+                CantBabys = this.CantBabys
+            };
+        }
 
     }
 }
