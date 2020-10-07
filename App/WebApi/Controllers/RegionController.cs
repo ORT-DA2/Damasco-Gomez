ccef0e7da868e43da1c5fcbd31eee39d9835b8b7
yuligomez <yulianagomezsilva@gmail.com>
2020-10-07 17:52:57 -0300

    refactoring regionController

--- App/WebApi/Controllers/RegionController.cs
+++ App/WebApi/Controllers/RegionController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace WebApi.Controllers
 {
+    [ApiController]
     [Route("api/regions")]
     [ServiceFilter(typeof(AuthorizationDIFilter))]
     public class RegionController : VidlyControllerBase
