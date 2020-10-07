467f012eff11df810d3fe2051cd5cd3ad3452219
yuligomez <yulianagomezsilva@gmail.com>
2020-10-07 15:04:41 -0300

    implement function TestisCorrectionToken

--- App/BusinessLogic/Logics/SessionLogic.cs
+++ App/BusinessLogic/Logics/SessionLogic.cs
@@ -4,6 +4,9 @@ namespace BusinessLogic.Logics
 {
     public class SessionLogic : ISessionLogic
     {
-        
+        public bool isCorrectionToken (string token)
+        {
+            return true;
+        }
     }
 }
