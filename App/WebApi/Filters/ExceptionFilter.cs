006ea282a334104dc67ff848528d310e83b413f5
yuligomez <yulianagomezsilva@gmail.com>
2020-10-07 17:45:39 -0300

    catch(Exception) in ExceptionFilter

--- App/WebApi/Filters/ExceptionFilter.cs
+++ App/WebApi/Filters/ExceptionFilter.cs
@@ -1,3 +1,5 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 
 
@@ -7,6 +9,20 @@ namespace Filters
 {
     public class ExceptionFilter : IExceptionFilter 
     {
-        
+        public void OnException(ExceptionContext context)
+        {
+            try
+            {
+                throw context.Exception;
+            }
+            catch(Exception)
+            {
+                context.Result = new ContentResult()
+                {
+                    StatusCode = 500,
+                    Content = "Server error"
+                };
+            }
+        }
     }
 }
