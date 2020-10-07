8f0c77c3306f41ce54c559608d20a3509b907b24
vdamasco <>
2020-10-07 20:30:01 -0300

    Route the new get in house controller

--- App/WebApi/Controllers/HouseController.cs
+++ App/WebApi/Controllers/HouseController.cs
@@ -97,7 +97,8 @@ namespace WebApi.Controllers
             this.houseLogic.Delete();
             return Ok();
         }
-        [HttpGet("/touristpoint")]
+        [Route("touristpoint")]
+        [HttpGet()]
         public IActionResult GetHousesBy([FromQuery]HouseSearchModel houseSearchModel)
         {
             HouseSearch houseSearch = houseSearchModel.ToEntity();
