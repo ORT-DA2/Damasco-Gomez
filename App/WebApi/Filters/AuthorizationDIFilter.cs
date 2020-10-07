e1ed70e06ea1ba361c2c939fc87d5c0f8fc9ab7f
yuligomez <yulianagomezsilva@gmail.com>
2020-10-07 15:45:25 -0300

    adding AuthorizationDIFilter contructor

--- App/WebApi/Filters/AuthorizationDIFilter.cs
+++ App/WebApi/Filters/AuthorizationDIFilter.cs
@@ -1,9 +1,17 @@
+using Contracts;
 using Microsoft.AspNetCore.Mvc.Filters;
 
 namespace Filters
 {
     public class AuthorizationDIFilter : IAuthorizationFilter
     {
+        private readonly ISessionLogic sessionsLogic;
+
+         public AuthorizationDIFilter(ISessionLogic sessions)
+        {
+            this.sessionsLogic = sessions;
+        }
+
         
     }
 }
