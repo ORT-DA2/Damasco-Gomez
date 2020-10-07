e5f5cad65bf4b2b92b14cc576d04174593a450d0
yuligomez <yulianagomezsilva@gmail.com>
2020-10-07 18:28:35 -0300

    improving error message

--- App/WebApi/Filters/AuthorizationAttributeFilter.cs
+++ App/WebApi/Filters/AuthorizationAttributeFilter.cs
@@ -1,10 +1,43 @@
 using System;
+using Contracts;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 
 namespace Filters
 {
     public class AuthorizationAttributeFilter : Attribute, IAuthorizationFilter
     {
-        
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            string token = context.HttpContext.Request.Headers["Authorization"];
+
+            if(String.IsNullOrEmpty(token))
+            {
+                context.Result = new ContentResult()
+                {
+                    StatusCode = 401,
+                    Content = "Unauthorized : A token is required"
+                };
+            }
+            else
+            {
+                var sessions = this.GetSessionLogic(context);
+                if(!sessions.isCorrectToken(token))
+                {
+                    context.Result = new ContentResult()
+                    {
+                        StatusCode = 403,
+                        Content = "Not authorized"
+                    };
+                }
+            }
+        }
+
+        private ISessionLogic GetSessionLogic(AuthorizationFilterContext context)
+        {
+            var sessionType = typeof(ISessionLogic);
+
+            return context.HttpContext.RequestServices.GetService(sessionType) as ISessionLogic;
+        }
     }
 }
