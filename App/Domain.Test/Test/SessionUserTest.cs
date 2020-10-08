841597378c9783a464fb83da6cd8a47d37c9ae0c
yuligomez <yulianagomezsilva@gmail.com>
2020-10-07 23:12:36 -0300

    add SessionUserTest

---
+++ App/Domain.Test/Test/SessionUserTest.cs
@@ -0,0 +1,18 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Domain.Test.Test
+{
+    [TestClass]
+    public class SessionUserTest
+    {
+        public void TestUpdateName()
+        {
+             Assert.IsTrue(true);
+        }
+        [TestMethod]
+        public void TestUpdateNameEmpty()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
