6abf2df4b5a67fa6a765d9313a5de5d9f0a7586d
yuligomez <yulianagomezsilva@gmail.com>
2020-10-09 19:40:58 -0300

    adding class SessionUserRepositoryTest

---
+++ App/DataAccess.Tests/Test/SessionUserRepositoryTest.cs
@@ -0,0 +1,19 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DataAccess.Tests.Test
+{
+     [TestClass]
+    public class SessionUserRepositoryTest
+    {
+        [TestMethod]
+        public void TestUpdateOk ()
+        {
+            Assert.IsTrue(true);
+        }
+        [TestMethod]
+        public void TestValidateok ()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
