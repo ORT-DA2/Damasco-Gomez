8e7b05f6b995a2e130ed73182f56c18d0d5d4bf8
yuligomez <yulianagomezsilva@gmail.com>
2020-10-09 22:53:37 -0300

    adding a new migration

---
+++ App/DataAccess/Migrations/20201010014934_SixteentMigration.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DataAccess.Migrations
+{
+    public partial class SixteentMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Sessions");
+
+            migrationBuilder.DropColumn(
+                name: "Password",
+                table: "Sessions");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Sessions",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Password",
+                table: "Sessions",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
