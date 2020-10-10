4e56f46e7aa5419b439f039f55fe845fd1100fb4
yuligomez <yulianagomezsilva@gmail.com>
2020-10-10 10:10:35 -0300

    quit comments

--- App/Domain/Entities/Region.cs
+++ App/Domain/Entities/Region.cs
@@ -7,7 +7,6 @@ namespace Domain
     {
         public int Id {get; set; }
         public string Name {get; set;}
-        //public virtual List<TouristPoint> TouristPoints {get; set;}
 
         public override bool Equals(object obj)
         {
@@ -21,7 +20,6 @@ namespace Domain
         public void Update(Region element)
         {
             if(element.Name != null) this.Name = element.Name;
-            //if(element.TouristPoints != null)  this.TouristPoints = element.TouristPoints;
         }
     }
 }
