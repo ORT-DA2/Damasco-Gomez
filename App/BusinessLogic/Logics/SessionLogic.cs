6af65d6aed8c78216fc09ea251d06018bd44209a
yuligomez <yulianagomezsilva@gmail.com>
2020-10-09 22:36:04 -0300

    new implementation of Login

--- App/BusinessLogic/Logics/SessionLogic.cs
+++ App/BusinessLogic/Logics/SessionLogic.cs
@@ -1,15 +1,22 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using Contracts;
 using DataAccessInterface.Repositories;
+using Domain;
+using Domain.Entities;
 
 namespace BusinessLogic.Logics
 {
     public class SessionLogic : ISessionLogic
     {
         private readonly ISessionUserRepository sessionUserRepository;
-       public SessionLogic(ISessionUserRepository sessionUserRepository)
+
+        private readonly IPersonRepository personRepository;
+       public SessionLogic(ISessionUserRepository sessionUserRepository, IPersonRepository personRepository)
        {
            this.sessionUserRepository = sessionUserRepository;
+           this.personRepository = personRepository;
        }
 
         public bool IsCorrectToken(Guid token)
@@ -17,9 +24,21 @@ namespace BusinessLogic.Logics
              return this.sessionUserRepository.IsCorrectToken(token);
 
         }
-        public void Login(string userName , string password)  
+        public void Login(Person person)  
         {
-            // crear session y generar token 
+            List<Person> personResult = this.personRepository.GetElements().FindAll(kz=>kz.Email == person.Email);
+            if (personResult.Count==0)
+            {
+                throw new ArgumentException("User was not created");
+            }
+            person = personResult.First();
+            Guid guid = Guid.NewGuid();
+            SessionUser newSession = new SessionUser ()
+            {
+                Token = guid,
+                PersonId = person.Id
+            };
+            this.sessionUserRepository.Add(newSession);
         }
     }
 }
