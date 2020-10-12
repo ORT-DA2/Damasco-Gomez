d397994a316afcb64ff0ab81b8774087c433beac
vdamasco <>
2020-10-12 18:19:34 -0300

    Remove try and catch from region controller

--- App/WebApi/Controllers/RegionController.cs
+++ App/WebApi/Controllers/RegionController.cs
@@ -32,80 +32,43 @@ namespace WebApi.Controllers
         [HttpGet( "{id}" , Name="GetRegion" )]
         public IActionResult GetBy([FromRoute]int id)
         {
-            try
-            {
-                var elementRegion = this.regionLogic.GetBy(id);
-                RegionDetailModel regionsBasic = new RegionDetailModel(elementRegion);
-                return Ok(regionsBasic);
-            }
-            catch (ArgumentException)
-            {
-                return BadRequest("There is not a booking with that id");
-            }
+            var elementRegion = this.regionLogic.GetBy(id);
+            RegionDetailModel regionsBasic = new RegionDetailModel(elementRegion);
+            return Ok(regionsBasic);
         }
-        [HttpPost()]
+        [HttpPost]
         [AuthorizationFilter]
         public IActionResult Post([FromBody]RegionModel regionModel)
         {
-            try
-            {
-                Region region = regionModel.ToEntity();
-                region = this.regionLogic.Add(region);
-                RegionDetailModel regionsBasic = new RegionDetailModel(region);
-                var creationRoute = CreatedAtRoute("GetRegion", new {Id = regionsBasic.Id} , regionsBasic);
-                return creationRoute;
-            }
-            catch (AggregateException)
-            {
-                return BadRequest("The region was already added");
-            }
-            catch (ArgumentException e)
-            {
-                return BadRequest("Error while validate : "+ e.Message.ToString());
-            }
-            catch (Exception e)
-            {
-                return BadRequest("The server had an error : "+ e.Message.ToString());
-            }
+            Region region = regionModel.ToEntity();
+            region = this.regionLogic.Add(region);
+            RegionDetailModel regionsBasic = new RegionDetailModel(region);
+            var creationRoute = CreatedAtRoute("GetRegion", new {Id = regionsBasic.Id} , regionsBasic);
+            return creationRoute;
         }
         [HttpPut("{id}")]
         [AuthorizationFilter]
         public IActionResult Put([FromRoute]int id,[FromBody]RegionModel regionModel)
         {
-            try
-            {
-                Region region = regionModel.ToEntity();
-                region = this.regionLogic.Update(id,region);
-                RegionDetailModel regionsBasic = new RegionDetailModel(region);
-                var creationRoute = CreatedAtRoute("GetRegion", new {Id = regionsBasic.Id} , regionsBasic);
-                return creationRoute;
-            }
-            catch(ArgumentException e)
-            {
-                return BadRequest("Error while validate :"+ e.Message.ToString());
-            }
-            catch (Exception e)
-            {
-                return BadRequest("Internal server error : "+ e.Message.ToString());
-            }
+            Region region = regionModel.ToEntity();
+            region = this.regionLogic.Update(id,region);
+            RegionDetailModel regionsBasic = new RegionDetailModel(region);
+            var creationRoute = CreatedAtRoute("GetRegion", new {Id = regionsBasic.Id} , regionsBasic);
+            return creationRoute;
         }
         [HttpDelete("{id}")]
         [AuthorizationFilter]
         public IActionResult Delete([FromRoute]int id)
         {
-            if (this.regionLogic.GetBy(id) == null)
-            {
-                return NotFound();
-            }
             this.regionLogic.Delete(id);
-            return Ok();
+            return Ok("Element was delete with id "+id);
         }
-        [HttpDelete()]
+        [HttpDelete]
         [AuthorizationFilter]
         public IActionResult Delete()
         {
             this.regionLogic.Delete();
-            return Ok();
+            return Ok("All data from Region was");
         }
     }
 }
