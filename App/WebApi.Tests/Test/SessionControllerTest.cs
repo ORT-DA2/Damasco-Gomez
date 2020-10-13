e43ff86aaa201b326e29153d52e2cbe4033e9460
yuligomez <yulianagomezsilva@gmail.com>
2020-10-13 18:59:52 -0300

    refactoring TestLoginOk in SessionController

--- App/WebApi.Tests/Test/SessionControllerTest.cs
+++ App/WebApi.Tests/Test/SessionControllerTest.cs
@@ -1,14 +1,43 @@
+using Contracts;
+using Domain;
+using Domain.Entities;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Model.In;
+using Moq;
+using WebApi.Controllers;
 
 namespace WebApi.Tests.Test
 {
     [TestClass]
     public class SessionControllerTest
     {
+       
+        private SessionUser sessionUser;
+        private Mock<ISessionLogic> mock;
+        private SessionController sessionController ;
+        private PersonModel personModel;
+        private Person person;
+
+        [TestInitialize]
+        public void initVariables()
+        {
+            PersonModel personModel = new PersonModel()
+            {
+                Email = "yuli@gmail.com",
+                Password ="uruguaynomas",
+            };
+            mock = new Mock<ISessionLogic>(MockBehavior.Strict);
+            sessionController = new SessionController(mock.Object);
+        }
         [TestMethod]
         public void TestLoginOk()
         {
-            Assert.IsTrue(true);
+            Person person = personModel.ToEntity();
+            Guid newToken = Guid.NewGuid();
+            mock.Setup(m => m.Login(person)).Returns(newToken);
+            var result = sessionController.Post(personModel);
+            mock.VerifyAll();
+            Assert.AreEquals(result,newToken);
         }
 
     }
