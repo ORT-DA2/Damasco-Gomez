6ef518d10a8226b8a940c762ab72e4e5125a729b
yuligomez <yulianagomezsilva@gmail.com>
2020-10-12 21:41:27 -0300

    CLEAN CODE - in DataAccess.Test

--- App/DataAccess.Tests/Test/PersonRepositoryTest.cs
+++ App/DataAccess.Tests/Test/PersonRepositoryTest.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using DataAccess.Context;
 using DataAccess.Repositories;
-using DataAccess.Tests.Utils;
 using Domain;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
