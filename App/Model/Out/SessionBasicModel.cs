73ce432fedf59803bd2befc0b708de77e0691419
vdamasco <>
2020-10-13 21:47:50 -0300

    Improve session test controller

--- App/Model/Out/SessionBasicModel.cs
+++ App/Model/Out/SessionBasicModel.cs
@@ -4,10 +4,19 @@ namespace Model.Out
 {
     public class SessionBasicModel
     {
-         public Guid Token {get; set;}
-           public SessionBasicModel(Guid token)
+        public Guid Token {get; set;}
+        public SessionBasicModel(Guid token)
         {
             this.Token = token;
+        }
+        public override bool Equals(object obj)
+        {
+            var result = false;
+            if(obj is SessionBasicModel session)
+            {
+                result = this.Token == session.Token ;
+            }
+            return result;
         }
     }
 }
