37cf3ae86c482f82439773a85453769ca2408861
vdamasco <>
2020-10-13 21:01:57 -0300

    Add config for the swagger

--- App/WebApi/Controllers/VidlyControllerBase.cs
+++ App/WebApi/Controllers/VidlyControllerBase.cs
@@ -2,6 +2,8 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace WebApi.Controllers
 {
+    [ApiExplorerSettings(IgnoreApi=true)]
+
     [ApiController]
     public class VidlyControllerBase : ControllerBase
     {
