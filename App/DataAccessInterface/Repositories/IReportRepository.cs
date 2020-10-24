3178b9e6f9a5e7a09dfc28c524947baa71365350
yuligomez <yulianagomezsilva@gmail.com>
2020-10-24 18:35:50 -0300

    refactoring FilterCantBookigsByHouses in ReportRepository

--- App/DataAccessInterface/Repositories/IReportRepository.cs
+++ App/DataAccessInterface/Repositories/IReportRepository.cs
@@ -1,7 +1,11 @@
+using System;
+using System.Collections.Generic;
+using Domain.Entities;
+
 namespace DataAccessInterface.Repositories
 {
     public class IReportRepository
     {
-        
+        List<Report> FilterCantBookigsByHouses (DateTime dateFrom, DateTime dateOn) ;
     }
 }
