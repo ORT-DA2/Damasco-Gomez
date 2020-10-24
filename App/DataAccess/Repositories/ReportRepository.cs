d7532b2c968e445a5502cc37625e1f6266969483
yuligomez <yulianagomezsilva@gmail.com>
2020-10-23 22:26:51 -0300

    adding implementations

--- App/DataAccess/Repositories/ReportRepository.cs
+++ App/DataAccess/Repositories/ReportRepository.cs
@@ -1,11 +1,22 @@
 using DataAccessInterface.Repositories;
+using Domain.Entities;
 
 namespace DataAccess.Repositories
 {
-    public class ReportRepository : IReportRepository
+    public class ReportRepository : AccessData<Report> /*, IReportRepository*/
     {
         public ReportRepository()
         {
+
+        }
+        protected override void Update(Report elementToUpdate, Report element)
+        {
+         
+        }
+
+        protected override void Validate(Report element)
+        {
+            
         }
     }
 }
