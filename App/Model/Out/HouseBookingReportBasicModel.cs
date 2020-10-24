99f22e2a361d50ac490eada81d49eb45ac6878fc
yuligomez <yulianagomezsilva@gmail.com>
2020-10-24 13:28:12 -0300

    adding  HouseBookingReportModel OUT first try

---
+++ App/Model/Out/HouseBookingReportBasicModel.cs
@@ -0,0 +1,30 @@
+using System.Diagnostics.CodeAnalysis;
+using Domain;
+using Domain.Entities;
+
+namespace Model.Out
+{
+    [ExcludeFromCodeCoverage]
+    public class HouseBookingReportBasicModel
+    {
+        public int Id {get ; set; }
+        public string HouseName {get ; set; }
+        public int cantBooking {get ; set; }
+
+         public override bool Equals(object obj)
+        {
+            var result = false;
+            if(obj is HouseBookingReportBasicModel house)
+            {
+                result = this.Id == house.Id ;
+            }
+            return result;
+        }
+       public HouseBookingReportBasicModel(House house, Report report)
+       {
+           this.Id= house.Id;
+           this.HouseName= house.Name;
+           this.cantBooking = 0; // calcular ese número 
+       }
+    }
+}
