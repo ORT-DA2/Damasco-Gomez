97040f0b6b236d4c09fa1c41e6bf6f8e2ae73a97
yuligomez <yulianagomezsilva@gmail.com>
2020-10-24 12:31:53 -0300

    adding reportRepository to logic

--- App/BusinessLogic/Logics/ReportLogic.cs
+++ App/BusinessLogic/Logics/ReportLogic.cs
@@ -1,12 +1,15 @@
 using BusinessLogicInterface;
 using BusinessLogicInterface.Interfaces;
+using DataAccessInterface.Repositories;
 
 namespace BusinessLogic.Logics
 {
     public class ReportLogic : IReportLogic
     {
-        public ReportLogic()
+        private readonly IReportRepository reportRepository;
+        public ReportLogic(IReportRepository reportRepository)
         {
+            this.reportRepository = reportRepository;
         }
     }
 }
