550d37f8162149b46b512949b669886b249f8d36
yuligomez <yulianagomezsilva@gmail.com>
2020-10-23 22:38:01 -0300

    adding dbSet y dbContext in ReportRepository

--- App/DataAccess/Repositories/ReportRepository.cs
+++ App/DataAccess/Repositories/ReportRepository.cs
@@ -1,22 +1,21 @@
 using DataAccessInterface.Repositories;
 using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace DataAccess.Repositories
 {
     public class ReportRepository : AccessData<Report> /*, IReportRepository*/
     {
+        public readonly DbSet<Report> dbSet;
+        public readonly DbContext context;
         public ReportRepository()
         {
-
         }
         protected override void Update(Report elementToUpdate, Report element)
         {
-         
         }
-
         protected override void Validate(Report element)
         {
-            
         }
     }
 }
