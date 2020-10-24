d9efac2e8044bc604ae56a4ff3bcd40662f78908
yuligomez <yulianagomezsilva@gmail.com>
2020-10-23 21:38:10 -0300

    add basic endpoint GetHousesReportBy

--- App/WebApi/Controllers/ReportController.cs
+++ App/WebApi/Controllers/ReportController.cs
@@ -1,7 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
 namespace WebApi.Controllers
 {
-    public class ReportController
+     [Route("api/reports")]
+    public class ReportController : VidlyControllerBase
     {
-        
+        [HttpGet]
+        public IActionResult GetHousesReportBy([FromQuery]int idTp, string dateFrom, string DateTo)
+        {
+            return Ok("devolver modelo hospedaje reserva");
+        }
     }
 }
