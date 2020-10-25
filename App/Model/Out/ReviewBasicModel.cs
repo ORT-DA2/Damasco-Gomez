e1f206bca7d9ecde2f8b73eba82809e40de4b353
vdamasco <>
2020-10-25 11:56:13 -0300

    Add models of review

---
+++ App/Model/Out/ReviewBasicModel.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics.CodeAnalysis;
+using Domain.Entities;
+
+namespace Model.Out
+{
+    [ExcludeFromCodeCoverage]
+    public class ReviewBasicModel
+    {
+        public int Id {get; private set; }
+        public string Name {get; private set;}
+        public string Comment {get; private set;}
+        public int HouseId {get; private set;}
+        public int Score {get; private set;}
+        public ReviewBasicModel(Review review)
+        {
+            this.Id = review.Id;
+            this.Name = review.Name;
+            this.Comment = review.Comment;
+            this.HouseId = review.HouseId;
+            this.Score = review.Score;
+        }
+        public override bool Equals(object obj)
+        {
+            var result = false;
+            if(obj is ReviewBasicModel review)
+            {
+                result = this.Id == review.Id ;
+            }
+            return result;
+        }
+    }
+}
