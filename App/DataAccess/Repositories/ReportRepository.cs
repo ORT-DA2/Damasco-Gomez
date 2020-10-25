0c7214ebe3e33f4316477a4013718464b6180722
yuligomez <yulianagomezsilva@gmail.com>
2020-10-25 11:32:03 -0300

    refactoring FilterCantBookigsByHouse orderBy createdOn

--- App/DataAccess/Repositories/ReportRepository.cs
+++ App/DataAccess/Repositories/ReportRepository.cs
@@ -22,7 +22,7 @@ namespace DataAccess.Repositories
         public List<Report> FilterCantBookigsByHouse (DateTime dateFrom, DateTime dateOut,int  idTp)
         {
             List <Report> housesAndCantBookings   = houses
-            .Where(h => h.TouristPointId == idTp)
+            .Where(h => h.TouristPointId == idTp).OrderByDescending(h=>h.CreatedOn)
                 .Select(
                     h => new Report(){ 
                         NameHouse = h.Name, 
