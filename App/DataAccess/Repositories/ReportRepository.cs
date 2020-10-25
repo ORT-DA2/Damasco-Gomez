1bbea2d69cd24281642a64833181b2b0ffa3d093
yuligomez <yulianagomezsilva@gmail.com>
2020-10-25 20:46:47 -0300

    refactoring filter

--- App/DataAccess/Repositories/ReportRepository.cs
+++ App/DataAccess/Repositories/ReportRepository.cs
@@ -32,7 +32,7 @@ namespace DataAccess.Repositories
                             (b.State.Name!="Rechazada") && 
                             (b.State.Name!="Expirada") && 
                             ((b.CheckIn <= dateFrom && b.CheckOut>=dateOut)|| 
-                            (b.CheckIn >dateFrom && b.CheckOut<=dateOut)|| 
+                            (b.CheckIn >dateFrom && b.CheckOut<=dateOut)|| (b.CheckIn >dateFrom && b.CheckOut>dateOut)|| 
                             (dateFrom < b.CheckIn && dateOut> b.CheckOut)))
                             .Count()
                         }
