25cc3e1d42e1f57838c6d2d71c3b03e139a578b6
vdamasco <>
2020-10-25 21:18:13 -0300

    Update front to be more to our system

--- Front/src/app/app.module.ts
+++ Front/src/app/app.module.ts
@@ -12,6 +12,7 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app.routing';
 import { ComponentsModule } from './components/components.module';
+import { UserLayoutComponent } from './layouts/user-layout/user-layout.component';
 
 
 @NgModule({
@@ -27,7 +28,8 @@ import { ComponentsModule } from './components/components.module';
   declarations: [
     AppComponent,
     AdminLayoutComponent,
-    AuthLayoutComponent
+    AuthLayoutComponent,
+    UserLayoutComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
