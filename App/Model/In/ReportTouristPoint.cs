62079b415a74e8ad234d623df7fa3f7014e7624a
yuligomez <yulianagomezsilva@gmail.com>
2020-10-25 22:01:12 -0300

    create ReportTouristPoint in to ReportController

---
+++ App/Model/In/ReportTouristPoint.cs
@@ -0,0 +1,12 @@
+using System.Diagnostics.CodeAnalysis;
+
+namespace Model.In
+{
+    [ExcludeFromCodeCoverage]
+    public class ReportTouristPoint
+    {
+        int idTp  {get; set;}
+        string dateFrom  {get; set;}
+        string dateOn  {get; set;} 
+    }
+}
