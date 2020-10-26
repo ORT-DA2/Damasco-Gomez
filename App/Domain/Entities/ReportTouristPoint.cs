7e328fae449c50e3b6c9bd8fb7f8f2f458e3ef54
yuligomez <yulianagomezsilva@gmail.com>
2020-10-25 22:03:14 -0300

    create ReportTouristPoint not persisted

---
+++ App/Domain/Entities/ReportTouristPoint.cs
@@ -0,0 +1,10 @@
+namespace Domain.Entities
+{
+    public class ReportTouristPoint
+    {
+         int idTp  {get; set;}
+        string dateFrom  {get; set;}
+        string dateOn  {get; set;} 
+
+    }
+}
