aff75635abd30bdfc5a03d720ff076af4aff43c1
yuligomez <yulianagomezsilva@gmail.com>
2020-10-25 22:08:33 -0300

    create ParseDateTime and checknotnull in model

--- App/Model/In/ReportTouristPointModel.cs
+++ App/Model/In/ReportTouristPointModel.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Diagnostics.CodeAnalysis;
 using Domain.Entities;
 
@@ -8,16 +9,39 @@ namespace Model.In
     {
         public int idTp  {get; set;}
         public string dateFrom  {get; set;}
-        public string dateOn  {get; set;} 
-         public ReportTouristPoint ToEntity()
+        public string dateOut  {get; set;} 
+        public ReportTouristPoint ToEntity()
         {
             return new ReportTouristPoint()
             {
                 idTp= this.idTp,
                 dateFrom = this.dateFrom,
-                dateOn= this.dateOn,
+                dateOut= this.dateOut,
 
             };
         }
+        public DateTime ParseDateTime(string dateString)
+        {
+            if (dateString.Length != 10 && !dateString.Contains("/")) throw new ArgumentException("Date is not in the right format");
+            string[] parse = dateString.Split('/');
+            int year = int.Parse(parse[2]);
+            int month = int.Parse(parse[1]);
+            int day = int.Parse(parse[0]);
+            DateTime date = new DateTime(year,month,day);
+            return date;
+        }
+        public bool NotNull()
+        {
+            bool notNull;
+            notNull = this.dateFrom != null || this.dateOut!=null;
+            return notNull;
+        }
+        public void CheckAllParameters()
+        {
+            bool allParams;
+            allParams = this.dateFrom != null && this.dateOut!=null && this.idTp > 0;
+            if (!allParams) throw new ArgumentException("You need to put dates and tourist point );
+        }
+
     }
 }
