e3ea7123a5df4c918af23411bb68e7fcfb552faf
yuligomez <yulianagomezsilva@gmail.com>
2020-10-25 21:58:16 -0300

    adding ReportController contructor

--- App/WebApi/Controllers/ReportController.cs
+++ App/WebApi/Controllers/ReportController.cs
@@ -1,4 +1,6 @@
 using System.Collections.Generic;
+using BusinessLogic.Logics;
+using BusinessLogicInterface.Interfaces;
 using Domain.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Model.In;
@@ -9,11 +11,17 @@ namespace WebApi.Controllers
     [Route("api/reports")]
     public class ReportController : VidlyControllerBase
     {
+        private IReportLogic reportLogic;
+         public ReportController(IReportLogic reportLogic)
+        {
+            this.reportLogic = reportLogic;
+        }
         [HttpGet]
         [AuthorizationFilter]
         public IActionResult GetHousesReportBy([FromQuery]int idTp, string dateFrom, string dateOn)
         {
             IEnumerable <Report> reportsToReturn;
+
             return Ok("reportsToReturn");
         }
     }
