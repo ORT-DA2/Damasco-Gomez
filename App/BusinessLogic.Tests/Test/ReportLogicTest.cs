d6d6a45a3f37be2613a5e643143f58bda4468924
yuligomez <yulianagomezsilva@gmail.com>
2020-10-25 21:51:10 -0300

    refactoring TestGetHousesReportByOk in ReportLogicTest

--- App/BusinessLogic.Tests/Test/ReportLogicTest.cs
+++ App/BusinessLogic.Tests/Test/ReportLogicTest.cs
@@ -1,14 +1,62 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using BusinessLogic.Logics;
+using DataAccessInterface.Repositories;
+using Domain.Entities;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 
 namespace BusinessLogic.Tests.Test
 {
     [TestClass]
     public class ReportLogicTest
     {
+        private ReportLogic reportLogic; 
+        private Mock<IReportRepository> mock;
+        private List<Report> reportsToReturn;
+        private List<Report> reportsEmpty;
+        [TestInitialize]
+            public void Initialize ()
+            {
+                reportsToReturn = new List<Report>()
+                {
+                    new Report()
+                    {
+                        CantBookings = 1,
+                        NameHouse = "Hotel L’Auberge",
+                    },
+                    new Report()
+                    {
+                        CantBookings = 2,
+                        NameHouse = "Hotel del Lago Golf & Art Resort",
+                    },
+                    new Report()
+                    {
+                        CantBookings = 3,
+                        NameHouse = "The Grand Hotel",
+                    },
+                    new Report()
+                    {
+                        CantBookings = 4,
+                        NameHouse = "Solanas Punta Del Este Spa & Resort (",
+                    }
+                };
+                mock = new Mock<IReportRepository>(MockBehavior.Strict);
+                reportLogic = new RegionLogic(mock.Object);
+                reportsEmpty = new List<Report>();
+            }
         [TestMethod]
         public void TestGetHousesReportByOk ()
         {
-            Assert.IsTrue(true);
+            int idTP = 1;
+            DateTime dateFrom = new DateTime(2020, 11, 29);
+            DateTime dateOut = new DateTime(2020, 12, 20);
+            mock.Setup(m => m.FilterCantBookigsByHouse(dateFrom,dateOut,idTP)).Returns(reportsToReturn);
+
+            List<Report> result = reportLogic.GetHousesReportBy(dateFrom,dateOut,idTp);
+
+            Assert.IsTrue(result.SequenceEqual(reportsToReturn));
         }
     }
 }
