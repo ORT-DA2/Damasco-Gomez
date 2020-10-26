928aef66aa8bd6262209c467e3d8ed19bc5dcb73
yuligomez <yulianagomezsilva@gmail.com>
2020-10-26 19:57:10 -0300

    ExcludeFromCodeCoverage ReportHousesBasicModel

--- App/Model/Out/ReportHousesBasicModel.cs
+++ App/Model/Out/ReportHousesBasicModel.cs
@@ -1,7 +1,9 @@
+using System.Diagnostics.CodeAnalysis;
 using Domain.Entities;
 
 namespace Model.Out
 {
+    [ExcludeFromCodeCoverage]
     public class ReportHousesBasicModel
     {
         public string NameHouse {get; set;}
