94fa430ed3c42374a6c4c7be84db7efd2a31093a
vdamasco <>
2020-10-26 22:09:46 -0300

    Add migration

--- App/DataAccess/Migrations/20201027010345_ThirtyOneMigration.Designer.cs
+++ App/DataAccess/Migrations/20201027010345_ThirtyOneMigration.Designer.cs
@@ -4,14 +4,16 @@ using DataAccess.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DataAccess.Migrations
 {
     [DbContext(typeof(VidlyContext))]
-    partial class VidlyContextModelSnapshot : ModelSnapshot
+    [Migration("20201027010345_ThirtyOneMigration")]
+    partial class ThirtyOneMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
