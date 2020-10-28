e93ed5fc96a371da6c7a14b320e2e2016dc6f667
vdamasco <>
2020-10-28 11:37:03 -0300

    Booking service

---
+++ Front/UruguayNatrualWeb/src/app/services/bookings/booking.service.ts
@@ -0,0 +1,35 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { BookingBasicInfo } from 'src/app/models/booking/booking-base-info';
+import { BookingDetailInfo } from 'src/app/models/booking/booking-detail-info';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BookingService {
+  private uri = environment.baseURL+"/bookings";
+  constructor(private http: HttpClient) { }
+  getAll():Observable<BookingBasicInfo[]>{
+    return this.http.get<BookingBasicInfo[]>(this.uri)
+      .pipe(catchError(this.handleError));
+  }
+  getBy(id):Observable<BookingDetailInfo>{
+    return this.http.get<BookingDetailInfo>(this.uri, {headers: id})
+  }
+  private handleError(error: HttpErrorResponse){
+    let message: string;
+    if (error.error instanceof ErrorEvent) {
+      message = "Error: do it again";
+    } else{
+      if(error.status == 0){
+        message = "The server is shutdown";
+      }else{
+        message = error.error.message;
+      }
+    }
+    return throwError(message);
+  }
+}
