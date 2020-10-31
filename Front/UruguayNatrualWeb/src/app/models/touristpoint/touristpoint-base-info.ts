6556ad26a51dd0f93eb64b086aa5ee25ab6f8f6e
yuligomez <yulianagomezsilva@gmail.com>
2020-10-31 18:52:05 -0300

    change TouristPointsBasicInfo model

--- Front/UruguayNatrualWeb/src/app/models/touristpoint/touristpoint-base-info.ts
+++ Front/UruguayNatrualWeb/src/app/models/touristpoint/touristpoint-base-info.ts
@@ -1,4 +1,5 @@
 //import {ImageTouristPointBasic} from './app/models/imagetouristpoint/imagetouristpoint-base-info';
+//import { ImageTouristPointBasic } from 'src/app/models/imagetouristpoint/imagetouristpoint-base-info';
 
 export interface TouristPointsBasicInfo {
   id : number,
