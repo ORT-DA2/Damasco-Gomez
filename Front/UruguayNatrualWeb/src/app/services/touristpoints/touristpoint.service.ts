27f62d3c68319c6479d1c5e4819aa61d45537c36
yuligomez <yulianagomezsilva@gmail.com>
2020-10-31 18:26:07 -0300

    change service recently added

--- Front/UruguayNatrualWeb/src/app/services/touristpoints/touristpoint.service.ts
+++ Front/UruguayNatrualWeb/src/app/services/touristpoints/touristpoint.service.ts
@@ -7,7 +7,7 @@ import { environment } from 'src/environments/environment';
 @Injectable({
   providedIn: 'root'
 })
-export class BookingService {
+export class TouristPointsService {
   private uri = environment.baseURL+'touristpoints';
   private id = 1;
   constructor(private http: HttpClient) { }
