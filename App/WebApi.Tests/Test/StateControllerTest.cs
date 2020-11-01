a6004314461363b43a910a003e5891f49022a659
vdamasco <valedamascoolaizola@gmail.com>
2020-11-01 11:48:49 -0300

    Create state controller test empty with methods empty

--- App/WebApi.Tests/Test/StateControllerTest.cs
+++ App/WebApi.Tests/Test/StateControllerTest.cs
@@ -1,7 +1,33 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
 namespace WebApi.Tests.Test
 {
+    [TestClass]
     public class StateControllerTest
     {
+        [TestInitialize]
+        public void initVariables()
+        {
+        }
+        [TestMethod]
+        public void TestGetAllStatesOk()
+        {
+        }
+
+        [TestMethod]
+        public void TestGetAllStatesEmpty()
+        {
+        }
+        [TestMethod]
+        public void TestGetByOk()
+        {
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestGetByNotFound()
+        {
+        }
         
     }
 }
--- App/WebApi/Controllers/StateController.cs
+++ App/WebApi/Controllers/StateController.cs
@@ -1,7 +1,35 @@
+using BusinessLogicInterface.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
 namespace WebApi.Controllers
 {
-    public class StateController
+    [Route("api/states")]
+    public class StateController : VidlyControllerBase
     {
+        private readonly IStateLogic stateLogic;
+
+        public StateController(IStateLogic stateLogic)
+        {
+            this.stateLogic = stateLogic;
+        }
+        /// <summary>
+        /// Permite a un usuario obtener información de todas los estdos del sistema
+        /// </summary>
+        /// <response code="200">Se devuelve la información requerida</response>
+        [HttpGet]
+        public IActionResult Get()
+        {
+        }
+        /// <summary>
+        /// Permite a un ususario ver un estado del sistema
+        /// </summary>
+        /// <param name="id">Este parámetro contiene el identificador de la state</param>
+        /// <response code="200">Se devuelve la información requerida.</response>
+        /// <response code="400">Categoria no existente con ese identificador</response>
+        [HttpGet( "{id}" , Name="GetState" )]
+        public IActionResult GetBy([FromRoute]int id)
+        {
+        }
         
     }
 }
