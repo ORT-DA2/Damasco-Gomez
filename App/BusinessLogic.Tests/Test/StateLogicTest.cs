5daa6ebedd6bfd3bf1eb2f29f5e8f1ab66972fae
vdamasco <valedamascoolaizola@gmail.com>
2020-11-01 11:31:12 -0300

    State logic test empty

---
+++ App/BusinessLogic.Tests/Test/StateLogicTest.cs
@@ -0,0 +1,30 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BusinessLogic.Tests.Test
+{
+    [TestClass]
+    public class StateLogicTest
+    {
+
+        [TestInitialize]
+        public void initVariables()
+        {
+        }
+        [TestMethod]
+        public void GetAll()
+        {
+        }
+        [TestMethod]
+        public void GetAllEmpty()
+        {
+        }
+        [TestMethod]
+        public void GetByTestOk()
+        {
+        }
+        [TestMethod]
+        public void TestGetByFail()
+        {
+        }
+    }
+}
