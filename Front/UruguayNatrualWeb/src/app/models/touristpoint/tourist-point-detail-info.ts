9879e8cbe6ba479313a36d7fb66001f4d53f16e9
vdamasco <valedamascoolaizola@gmail.com>
2020-11-03 09:03:05 -0300

    Add tourist point detail interface

--- Front/UruguayNatrualWeb/src/app/models/touristpoint/tourist-point-detail-info.ts
+++ Front/UruguayNatrualWeb/src/app/models/touristpoint/tourist-point-detail-info.ts
@@ -1,13 +1,13 @@
-
+import { CateogryBasicInfo } from "../category/category-basic-info";
 import { ImageTouristPointBasic } from "../imagetouristpoint/Imagetourispoint-base-info";
 import { RegionBasicInfo } from "../regions/region-base-info";
 
-export interface TouristPointsBasicInfo {
+export interface TouristPointDetailInfo {
   id : number,
   name : string,
   image : ImageTouristPointBasic,
   decsription : string,
   regionId : number,
   region : RegionBasicInfo,
-
+  categories : CateogryBasicInfo[],
 }
