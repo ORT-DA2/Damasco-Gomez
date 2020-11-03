70d0f1ef9eeac433d35ba4381d553233cde7ea56
vdamasco <valedamascoolaizola@gmail.com>
2020-11-03 09:34:04 -0300

    Add logic and html to the tourist point table

--- Front/UruguayNatrualWeb/src/app/pages/tourist-point/tourist-points-table/tourist-points-table.component.ts
+++ Front/UruguayNatrualWeb/src/app/pages/tourist-point/tourist-points-table/tourist-points-table.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { TouristPointsBasicInfo } from 'src/app/models/touristpoint/touristpoint-base-info';
+import { TouristPointsService } from 'src/app/services/touristpoints/touristpoint.service';
 
 @Component({
   selector: 'app-tourist-points-table',
@@ -6,10 +8,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tourist-points-table.component.css']
 })
 export class TouristPointsTableComponent implements OnInit {
+  public touristpoints: TouristPointsBasicInfo[] = [];
+  public id: Number = 0;
 
-  constructor() { }
+  constructor(private touristPointService: TouristPointsService) { }
 
   ngOnInit(): void {
+    this.touristPointService.getAll().subscribe(
+      touristPointResponse =>
+        this.getAll(touristPointResponse), (error: string) => this.showError(error)
+    );
+  }
+
+  private getAll(touristPointResponse: TouristPointsBasicInfo[]){
+    this.touristpoints = touristPointResponse;
+  }
+
+  private showError(message: string){
+    console.log(message);
+  }
+
+  private delete(event) {
+    console.log(event);
+    this.id = event.id;
+    this.touristPointService.delete(this.id).subscribe(
+      touristPointResponse =>
+        this.delete(touristPointResponse), (error: string) => this.showError(error)
+    );
   }
 
 }
