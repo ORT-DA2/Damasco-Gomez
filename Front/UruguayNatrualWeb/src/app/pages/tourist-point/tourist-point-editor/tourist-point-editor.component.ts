7a62ae0637d9f3541a8cf07a0a4a7dcf0ab54d07
vdamasco <valedamascoolaizola@gmail.com>
2020-11-03 18:00:42 -0300

    Add html in the tourist point editor

--- Front/UruguayNatrualWeb/src/app/pages/tourist-point/tourist-point-editor/tourist-point-editor.component.ts
+++ Front/UruguayNatrualWeb/src/app/pages/tourist-point/tourist-point-editor/tourist-point-editor.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CategoryBasicInfo } from 'src/app/models/category/category-basic-info';
+import { RegionBasicInfo } from 'src/app/models/regions/region-base-info';
+import { TouristPointDetailInfo } from 'src/app/models/touristpoint/tourist-point-detail-info';
+import { TouristPointsBasicInfo } from 'src/app/models/touristpoint/touristpoint-base-info';
+import { CategoryService } from 'src/app/services/categories/category.service';
+import { RegionService } from 'src/app/services/regions/region.service';
+import { TouristPointsService } from 'src/app/services/touristpoints/touristpoint.service';
 
 @Component({
   selector: 'app-tourist-point-editor',
@@ -6,10 +14,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tourist-point-editor.component.css']
 })
 export class TouristPointEditorComponent implements OnInit {
+  public touristPoint: TouristPointDetailInfo = {} as TouristPointDetailInfo;
+  public regions: RegionBasicInfo[] = [];
+  public states: CategoryBasicInfo[] = [];
+  public touristPointId: number = 0;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private touristPointService: TouristPointsService,
+    private regionService: RegionService,
+    private categoryService: CategoryService
+  ) { }
 
   ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.touristPointId = Number(id);
+    this.touristPointService.getBy(this.touristPointId).subscribe(
+      touristPointResponse =>
+        this.getBy(touristPointResponse), (error: string) => this.showError(error));
+    this.regionService.getAll().subscribe(
+      regionResponse =>
+        this.getAllRegions(regionResponse), (error: string) =>this.showError(error));
+    this.categoryService.getAll().subscribe(
+      categoryResponse =>
+        this.getAllCategories(categoryResponse), (error: string) =>this.showError(error));
+  }
+
+  private getBy(touristPointResponse: TouristPointDetailInfo){
+    this.touristPoint = touristPointResponse;
+  }
+  private getAllRegions(regionResponse: RegionBasicInfo[]){
+    this.regions = regionResponse;
+  }
+  private getAllCategories(categoryResponse: CategoryBasicInfo[]){
+    this.states = categoryResponse;
+  }
+
+  private showError(message: string){
+    console.log(message);
   }
 
 }
