c6cc9990c6d56e04ad842bbecb5f1439156c24a5
vdamasco <valedamascoolaizola@gmail.com>
2020-11-03 09:03:24 -0300

    Add page tourist point with components

---
+++ Front/UruguayNatrualWeb/src/app/pages/tourist-point/tourist-point-editor/tourist-point-editor.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tourist-point-editor',
+  templateUrl: './tourist-point-editor.component.html',
+  styleUrls: ['./tourist-point-editor.component.css']
+})
+export class TouristPointEditorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ Front/UruguayNatrualWeb/src/app/pages/tourist-point/tourist-points-table/tourist-points-table.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tourist-points-table',
+  templateUrl: './tourist-points-table.component.html',
+  styleUrls: ['./tourist-points-table.component.css']
+})
+export class TouristPointsTableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
