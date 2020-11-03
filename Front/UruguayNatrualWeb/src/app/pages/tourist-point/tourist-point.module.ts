4986c5b02855e436c9d9f5432e5dcdeff5c5bd42
vdamasco <valedamascoolaizola@gmail.com>
2020-11-03 09:33:49 -0300

    Import all component into the tourist point module

--- Front/UruguayNatrualWeb/src/app/pages/tourist-point/tourist-point.module.ts
+++ Front/UruguayNatrualWeb/src/app/pages/tourist-point/tourist-point.module.ts
@@ -1,12 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TouristPointEditorComponent } from './tourist-point-editor/tourist-point-editor.component';
+import { TouristPointsTableComponent } from './tourist-points-table/tourist-points-table.component';
+import { TouristPointDashboardComponent } from './tourist-point-dashboard/tourist-point-dashboard.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 
 @NgModule({
-  declarations: [],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    TouristPointModule,
+    BrowserModule,
+    ReactiveFormsModule,
+  ],
+  declarations: [
+    TouristPointEditorComponent,
+    TouristPointsTableComponent,
+    TouristPointDashboardComponent
+  ],
+  providers: [],
 })
 export class TouristPointModule { }
