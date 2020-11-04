72852541a64562cd648a7c98e33b30cf91112b41
yuligomez <yulianagomezsilva@gmail.com>
2020-11-04 20:51:31 -0300

    improving login

--- Front/UruguayNatrualWeb/src/app/pages/login/login.component.ts
+++ Front/UruguayNatrualWeb/src/app/pages/login/login.component.ts
@@ -1,23 +1,28 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { SessionService } from 'src/app/services/sessions/session.service';
 import {SessionUserModel} from '../../models/sessions/session-user-model';
-
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit, OnDestroy {
-  constructor() {}
+
+  constructor(private sessionService: SessionService) {}
 
   sessionUser: SessionUserModel = new SessionUserModel();
+
   ngOnInit() {
   }
 
   login(form: NgForm) {
 
     if (form.invalid) {return; }
-    console.log(form);
+    this.sessionService.login(this.sessionUser).
+      subscribe(resp => {
+          console.log(resp);
+        });
   }
   ngOnDestroy() {
   }
