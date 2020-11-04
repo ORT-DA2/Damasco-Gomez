def1769ec95267f404fcab5a67d59cf7a81acd3b
yuligomez <yulianagomezsilva@gmail.com>
2020-11-04 19:24:25 -0300

    crate function newUser in person service

--- Front/UruguayNatrualWeb/src/app/services/persons/person.service.ts
+++ Front/UruguayNatrualWeb/src/app/services/persons/person.service.ts
@@ -5,26 +5,45 @@ import { catchError } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 import { PersonBasicInfo } from 'src/app/models/person/person-base-info';
 
+
 @Injectable({
   providedIn: 'root'
 })
-export class PersonService {
-  private uri = environment.baseURL+'persons';
+export class SessionService {
+  private uri = environment.baseURL+ 'sessions';
   private id = 1;
+  private token;
   constructor(private http: HttpClient) { }
-  getAll():Observable<PersonBasicInfo[]>{
+
+  getAll(): Observable<PersonBasicInfo[]>{
     return this.http.get<PersonBasicInfo[]>(this.uri)
       .pipe(catchError(this.handleError));
   }
+  logout(){}
+
+  newUser(user: PersonBasicInfo){
+
+    const authData = {
+      ...user,
+      returnSecureToken: true
+    };
+    return this.http.post(
+      `${ this.uri}/persons`,
+      authData
+    );
+  }
+
+
 
   private handleError(error: HttpErrorResponse){
     let message: string;
     if (error.error instanceof ErrorEvent) {
       message = 'Error: do it again';
     } else{
+      // tslint:disable-next-line: triple-equals
       if(error.status == 0){
-        message = "The server is shutdown";
-      } else {
+        message = 'The server is shutdown';
+      } else{
         message = error.error.message;
       }
     }
