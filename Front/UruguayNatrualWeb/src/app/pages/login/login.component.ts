a8174c775eb21b93c45f31566a1399cd793ea39a
yuligomez <yulianagomezsilva@gmail.com>
2020-11-03 22:27:34 -0300

    more implementation in login page

--- Front/UruguayNatrualWeb/src/app/pages/login/login.component.ts
+++ Front/UruguayNatrualWeb/src/app/pages/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import {SessionUserModel} from '../../models/sessions/session-user-model';
 
 @Component({
   selector: 'app-login',
@@ -8,7 +10,14 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 export class LoginComponent implements OnInit, OnDestroy {
   constructor() {}
 
+  sessionUser: SessionUserModel = new SessionUserModel();
   ngOnInit() {
+  }
+
+  login(form: NgForm) {
+
+    if (form.invalid) {return; }
+    console.log(form);
   }
   ngOnDestroy() {
   }
