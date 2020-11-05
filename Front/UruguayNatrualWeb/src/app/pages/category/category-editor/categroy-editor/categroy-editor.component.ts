6c7aa845aaf24cc3f9de9f5d67b6c4606427694c
yuligomez <yulianagomezsilva@gmail.com>
2020-11-05 19:39:05 -0300

    crate catgegory-editor components

---
+++ Front/UruguayNatrualWeb/src/app/pages/category/category-editor/categroy-editor/categroy-editor.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-categroy-editor',
+  templateUrl: './categroy-editor.component.html',
+  styleUrls: ['./categroy-editor.component.css']
+})
+export class CategroyEditorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
