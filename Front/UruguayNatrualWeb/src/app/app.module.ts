350b1ee5439b34b4c810a94a242e0adbe79e5b69
yuligomez <yulianagomezsilva@gmail.com>
2020-11-05 20:21:34 -0300

    quit unnecessary imports at app.module

--- Front/UruguayNatrualWeb/src/app/app.module.ts
+++ Front/UruguayNatrualWeb/src/app/app.module.ts
@@ -21,9 +21,7 @@ import {RegionService} from './services/regions/region.service';
 import {ReportService} from './services/reports/report.service';
 import {ReviewService} from './services/reviews/review.service';
 import {SessionService} from './services/sessions/session.service';
-import { CategoryDashboardComponent } from './pages/category/category-dashboard/category-dashboard.component';
-import { CategoryTableComponent } from './pages/category/category-table/category-table/category-table.component';
-import { CategroyEditorComponent } from './pages/category/category-editor/categroy-editor/categroy-editor.component';
+
 
 @NgModule({
   imports: [
@@ -42,9 +40,6 @@ import { CategroyEditorComponent } from './pages/category/category-editor/categr
     AppComponent,
     AdminLayoutComponent,
     AuthLayoutComponent,
-    CategoryDashboardComponent,
-    CategoryTableComponent,
-    CategroyEditorComponent,
   ],
   providers: [TouristPointsService,
     CategoryService,
