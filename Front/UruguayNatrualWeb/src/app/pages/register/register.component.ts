1e556b515471dc62aedf44dce8b7ec8b96183dce
yuligomez <yulianagomezsilva@gmail.com>
2020-11-05 18:09:23 -0300

    adding login in onSubmit at register component

--- Front/UruguayNatrualWeb/src/app/pages/register/register.component.ts
+++ Front/UruguayNatrualWeb/src/app/pages/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { PersonBasicInfo } from 'src/app/models/person/person-base-info';
+import { SessionService } from 'src/app/services/sessions/session.service';
 import { PersonService } from '../../services/persons/person.service';
 
 @Component({
@@ -12,17 +13,19 @@ export class RegisterComponent implements OnInit {
 
   public user: PersonBasicInfo = {} as PersonBasicInfo;
 
-  constructor(private personService: PersonService) { }
+  constructor(private personService: PersonService, private sessionService: SessionService) { }
 
   ngOnInit() {
   }
 
   onSubmit(form: NgForm) {
     if (form.invalid) {return; }
-    this.personService.newUser(this.user).
-      subscribe(resp => {
-          console.log(resp);
-        });
+    this.personService.newUser(this.user);
+    this.sessionService.login(this.user).
+    subscribe(resp => {
+        console.log(resp);
+      });
+
   }
 
 }
