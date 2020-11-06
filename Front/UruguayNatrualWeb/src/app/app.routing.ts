3eb4425c4a00f0af8ec8d56cd048b330c8266dfe
yuligomez <yulianagomezsilva@gmail.com>
2020-11-06 20:20:58 -0300

    adding canActivate to app routing

--- Front/UruguayNatrualWeb/src/app/app.routing.ts
+++ Front/UruguayNatrualWeb/src/app/app.routing.ts
@@ -5,6 +5,7 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
 import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
+import { AdminGuard } from './guards/admin.guard';
 
 const routes: Routes =[
   {
@@ -13,7 +14,7 @@ const routes: Routes =[
     pathMatch: 'full',
   }, {
     path: '',
-    component: AdminLayoutComponent,
+    component: AdminLayoutComponent, canActivate: [AdminGuard],
     children: [
       {
         path: '',
