bc5730d562d0a408c1b53e4880c1a7b06efa26ac
yuligomez <yulianagomezsilva@gmail.com>
2020-11-07 19:46:57 -0300

    adding getall to category table .ts

--- Front/UruguayNatrualWeb/src/app/pages/category/category-table/category-table.component.ts
+++ Front/UruguayNatrualWeb/src/app/pages/category/category-table/category-table.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CategoryBasicInfo } from 'src/app/models/category/category-basic-info';
+import { CategoryService } from 'src/app/services/categories/category.service';
 
 @Component({
   selector: 'app-category-table',
@@ -6,10 +8,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./category-table.component.css']
 })
 export class CategoryTableComponent implements OnInit {
+  public categories: CategoryBasicInfo[] = [];
+  constructor(private categoryService: CategoryService) { }
 
-  constructor() { }
 
   ngOnInit(): void {
+    this.categoryService.getAll().subscribe(
+      response =>
+        this.getAll(response), (error: string) => this.showError(error)
+    );
   }
+
+  private getAll(response: CategoryBasicInfo[]){
+    this.categories = response;
+  }
+
+  private showError(message: string){
+    console.log(message);
+  }
+
 
 }
