98c174bdbcdd51bd0306e49b8dca78e8d1b73705
vdamasco <valedamascoolaizola@gmail.com>
2020-11-07 17:36:17 -0300

    remove comment

--- Front/UruguayNatrualWeb/src/app/services/sessions/session.service.ts
+++ Front/UruguayNatrualWeb/src/app/services/sessions/session.service.ts
@@ -57,7 +57,6 @@ export class SessionService {
     if (error.error instanceof ErrorEvent) {
       message = 'Error: do it again';
     } else{
-      // tslint:disable-next-line: triple-equals
       if (error.status == 0){
         message = 'The server is shutdown';
       } else{
