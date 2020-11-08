c2a6a1e9910cd7a954ee62e0b167baf20a5a0b1f
yuligomez <yulianagomezsilva@gmail.com>
2020-11-08 16:55:54 -0300

    create new component editor to add categorys

---
+++ Front/UruguayNatrualWeb/src/app/pages/category/category-editor-new/category-editor-new.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-category-editor-new',
+  templateUrl: './category-editor-new.component.html',
+  styleUrls: ['./category-editor-new.component.css']
+})
+export class CategoryEditorNewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
