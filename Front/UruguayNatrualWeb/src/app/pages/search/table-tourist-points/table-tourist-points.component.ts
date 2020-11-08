6a4433b47ee6e1f1d0f805d0e8e83ae8251a97b4
vdamasco <valedamascoolaizola@gmail.com>
2020-11-08 13:12:48 -0300

    Update table tourist point

--- Front/UruguayNatrualWeb/src/app/pages/search/table-tourist-points/table-tourist-points.component.ts
+++ Front/UruguayNatrualWeb/src/app/pages/search/table-tourist-points/table-tourist-points.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { TouristPointsBasicInfo } from 'src/app/models/touristpoint/touristpoint-base-info';
+import { TouristPointsService } from 'src/app/services/touristpoints/touristpoint.service';
 
 @Component({
   selector: 'app-table-tourist-points',
@@ -6,15 +8,28 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./table-tourist-points.component.css']
 })
 export class TableTouristPointsComponent implements OnInit {
+  public touristPoints: TouristPointsBasicInfo[] = [];
 
-  @Input()
   public by : string;
-  @Input()
   public id : string;
 
-  constructor() { }
+  constructor(private touristPointService: TouristPointsService) { }
 
   ngOnInit(): void {
+    this.touristPointService.getAll().subscribe(
+      touristPointResponse =>
+        this.getAll(touristPointResponse), (error: string) => this.showError(error)
+    );
   }
 
+  private getAll(touristPointResponse: TouristPointsBasicInfo[]){
+    this.touristPoints = touristPointResponse;
+  }
+
+  private showError(message: string){
+    console.log(message);
+  }
+  chooseRegion(event){
+    console.log(event);
+  }
 }
