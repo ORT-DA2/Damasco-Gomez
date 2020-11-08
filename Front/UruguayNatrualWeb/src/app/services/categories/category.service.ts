7e2c80ad23f3f94d061e17111067e51103a7e793
yuligomez <yulianagomezsilva@gmail.com>
2020-11-08 15:44:46 -0300

    adding token Authorization at category service

--- Front/UruguayNatrualWeb/src/app/services/categories/category.service.ts
+++ Front/UruguayNatrualWeb/src/app/services/categories/category.service.ts
@@ -37,13 +37,26 @@ export class CategoryService {
     return this.http.get<CategoryDetailInfo>(this.uri + '/' + id)
       .pipe(catchError(this.handleError));
   }
-  delete( id ): Observable<any> {
-
-    return this.http.delete(`${ this.uri}${ id }.json`).pipe(catchError(this.handleError));
-
+  delete(id):Observable<any>{
+    const headers = new HttpHeaders({
+      'Authorization':  localStorage.getItem('token'),
+      'Content-Type':'application/json'
+    });
+    let options = { headers: headers };
+    var httpRequest = this.http.delete<any>(this.uri + '/' + id,options)
+      .pipe(catchError(this.handleError));
+    return httpRequest;
   }
+
   update(id, body):Observable<any>{
-
-    return this.http.put(`${ this.uri }${ id }.json`, body).pipe(catchError(this.handleError));
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': localStorage.getItem('token') ,
+    });
+    let options = { headers: headers };
+    var httpRequest = this.http.put<any>(this.uri + '/' + id, body, options)
+      .pipe(catchError(this.handleError));
+    return httpRequest;
   }
+
 }
