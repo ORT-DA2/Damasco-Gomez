f72a7f14b9813dbc775094e4c4ee6c5066a24bdf
yuligomez <yulianagomezsilva@gmail.com>
2020-11-08 14:10:22 -0300

    inyect service in category editor constructor

--- Front/UruguayNatrualWeb/src/app/pages/category/category-editor/category-editor.component.ts
+++ Front/UruguayNatrualWeb/src/app/pages/category/category-editor/category-editor.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CategoryBasicInfo } from 'src/app/models/category/category-basic-info';
+import { CategoryDetailInfo } from 'src/app/models/category/category-detail-info';
+import { TouristPointsBasicInfo } from 'src/app/models/touristpoint/touristpoint-base-info';
+import { CategoryService } from 'src/app/services/categories/category.service';
+import { TouristPointsService } from 'src/app/services/touristpoints/touristpoint.service';
 
 @Component({
   selector: 'app-category-editor',
@@ -6,8 +12,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./category-editor.component.css']
 })
 export class CategoryEditorComponent implements OnInit {
+  public category: CategoryDetailInfo = {} as CategoryDetailInfo;
+  public touristPoints: TouristPointsBasicInfo[] = [];
+  public categoryId: number = 0;
+  tourisPointNew: TouristPointsBasicInfo = {} as TouristPointsBasicInfo;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private categoryService: CategoryService,
+    private touristPointService: TouristPointsService) { }
 
   ngOnInit(): void {
   }
