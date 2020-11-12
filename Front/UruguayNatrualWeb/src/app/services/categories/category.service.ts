facb314d83dead48d435b2afdc9c815a07e2db2a
yuligomez <yulianagomezsilva@gmail.com>
2020-11-11 21:23:50 -0300

    adding method add to category service

--- Front/UruguayNatrualWeb/src/app/services/categories/category.service.ts
+++ Front/UruguayNatrualWeb/src/app/services/categories/category.service.ts
@@ -59,4 +59,16 @@ export class CategoryService {
     return httpRequest;
   }
 
+  add(id, body):Observable<any>{
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization':  localStorage.getItem('token')
+    });
+    let options = { headers: headers };
+    var httpRequest = this.http.post<any>(this.uri + '/' + id, body, options)
+      .pipe(catchError(this.handleError));
+    return httpRequest;
+  }
+
+
 }
