4741d8a83dee908df01644a32d00ac9143f31d22
yuligomez <yulianagomezsilva@gmail.com>
2020-11-14 18:13:45 -0300

    adding  HouseDetailInfo

---
+++ Front/UruguayNatrualWeb/src/app/models/house/house-detail-info.ts
@@ -0,0 +1,18 @@
+import { TouristPointsBasicInfo } from "../touristpoint/touristpoint-base-info";
+
+export interface HouseDetailInfo {
+  id : number,
+  avaiable : boolean,
+  pricePerNight : number,
+  touristPointId : number,
+  name : string,
+  starts : number,
+  address : string,
+  //image : ImageBasicModel,
+  description : string,
+  phone : number,
+  contact: string,
+  totalPrice: number,
+  touristPoint :  TouristPointsBasicInfo ;
+
+}
