f9a80deea82b0ee54cd0c76421f01be410c262de
vdamasco <valedamascoolaizola@gmail.com>
2020-11-15 15:09:34 -0300

    Getting houses by tp id , dates and people

--- Front/UruguayNatrualWeb/src/app/pages/search/house-search/house-search.component.ts
+++ Front/UruguayNatrualWeb/src/app/pages/search/house-search/house-search.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { HouseBasicInfo } from 'src/app/models/house/house-base-info';
@@ -12,6 +12,16 @@ import { HouseService } from 'src/app/services/houses/house.service';
 export class HouseSearchComponent implements OnInit {
   panelOpenState = false;
   public houses: HouseBasicInfo[] = {} as HouseBasicInfo[];
+  touristPointId: string;
+  public checkIn: string;
+  public checkOut: string;
+  checkInValue : string;
+  checkOutValue : string;
+  public cantAdults: string = '0';
+  public cantChildren: string = '0';
+  public cantBabies: string = '0';
+  public cantSeniors: string = '0';
+  public searchDone: boolean = false;
 
   public myForm: FormGroup;
 
@@ -20,19 +30,37 @@ export class HouseSearchComponent implements OnInit {
     private houseService: HouseService) { }
 
   ngOnInit(): void {
-    const id = this.route.snapshot.paramMap.get('id');
-    this.houseService.getAll().subscribe(
-      houseResponse =>
-        this.getAll(houseResponse), (error: string) => this.showError(error)
-    );
+    this.touristPointId = (this.route.snapshot.paramMap.get('id'));
   }
-  private getAll(houseResponse: HouseBasicInfo[]){
+  private getAll(houseResponse: HouseBasicInfo[]) {
     this.houses = houseResponse;
+    this.searchDone = true;
   }
 
-  private showError(message: string){
+  private showError(message: string) {
     console.log(message);
   }
 
+  searchBy() {
+    this.houseService.getByTouristoPoint(this.checkInValue, this.checkOutValue, this.touristPointId,
+      this.cantAdults, this.cantChildren, this.cantBabies, this.cantSeniors).subscribe(
+        houseResponse =>
+          this.getAll(houseResponse), (error: string) => this.showError(error)
+      );
+  }
 
+  onChangeCheckIn(event){
+    this.checkInValue = this.parseDate(event);
+  }
+  onChangeCheckOut(event){
+    this.checkOutValue = this.parseDate(event);
+  }
+
+  private parseDate(date: string){
+    var dates = date.split('-');
+    const year = dates.length == 3 ? dates[0] : '0';
+    const month = dates.length == 3 ? dates[1] : '0';
+    const day = dates.length == 3 ? dates[2] : '0';
+    return day + '/' + month + '/' + year ;
+  }
 }
--- Front/UruguayNatrualWeb/src/app/services/houses/house.service.ts
+++ Front/UruguayNatrualWeb/src/app/services/houses/house.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
@@ -10,29 +10,45 @@ import { HouseDetailInfo } from 'src/app/models/house/house-detail-info';
   providedIn: 'root'
 })
 export class HouseService {
-  private uri = environment.baseURL+'houses';
+  private uri = environment.baseURL + 'houses';
   private id = 1;
   constructor(private http: HttpClient) { }
 
-  getAll():Observable<HouseBasicInfo[]>{
+  getAll(): Observable<HouseBasicInfo[]> {
     return this.http.get<HouseBasicInfo[]>(this.uri)
       .pipe(catchError(this.handleError));
   }
-  delete(id):Observable<any>{
+
+  getByTouristoPoint(CheckIn: string, CheckOut: string,
+    TouristPointId: string, CantAdults: string,
+    CantChildren: string, CantBabies: string, CantSeniors: string)
+    : Observable<HouseBasicInfo[]> {
+    let params = new HttpParams()
+      .set('checkin', CheckIn)
+      .set('checkout', CheckOut)
+      .set('touristpointId', TouristPointId)
+      .set('cantadults', CantAdults)
+      .set('cantchildrens', CantChildren)
+      .set('cantbabys', CantBabies)
+      .set('cantSeniors', CantSeniors);
+    return this.http.get<HouseBasicInfo[]>(this.uri, { params });
+  }
+
+  delete(id): Observable<any> {
     const headers = new HttpHeaders({
-      'Authorization':  localStorage.getItem('token'),
-      'Content-Type':'application/json'
+      'Authorization': localStorage.getItem('token'),
+      'Content-Type': 'application/json'
     });
     let options = { headers: headers };
-    var httpRequest = this.http.delete<any>(this.uri + '/' + id,options)
+    var httpRequest = this.http.delete<any>(this.uri + '/' + id, options)
       .pipe(catchError(this.handleError));
     return httpRequest;
 
   }
-  updateAvailable(id, body){
+  updateAvailable(id, body) {
     const headers = new HttpHeaders({
       'Content-Type': 'application/json',
-      'Authorization':  localStorage.getItem('token')
+      'Authorization': localStorage.getItem('token')
     });
     let options = { headers: headers };
     var httpRequest = this.http.put<any>(this.uri + '/' + id, body, options)
@@ -40,29 +56,29 @@ export class HouseService {
     return httpRequest;
   }
 
-  add(body):Observable<any>{
+  add(body): Observable<any> {
     const headers = new HttpHeaders({
       'Content-Type': 'application/json',
-      'Authorization':  localStorage.getItem('token')
+      'Authorization': localStorage.getItem('token')
     });
     let options = { headers: headers };
-    var httpRequest = this.http.post<any>(this.uri , body, options)
+    var httpRequest = this.http.post<any>(this.uri, body, options)
       .pipe(catchError(this.handleError));
     return httpRequest;
   }
 
-  getBy (id): Observable<HouseDetailInfo>{
+  getBy(id): Observable<HouseDetailInfo> {
     return this.http.get<HouseDetailInfo>(this.uri + '/' + id)
       .pipe(catchError(this.handleError));
   }
-  private handleError(error: HttpErrorResponse){
+  private handleError(error: HttpErrorResponse) {
     let message: string;
     if (error.error instanceof ErrorEvent) {
       message = 'Error: do it again';
-    } else{
-      if(error.status == 0){
+    } else {
+      if (error.status == 0) {
         message = 'The server is shutdown';
-      }else{
+      } else {
         message = error.error.message;
       }
     }
