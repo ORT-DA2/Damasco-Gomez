5aae002d2984909565ef610741c78ecc46eea8f9
vdamasco <valedamascoolaizola@gmail.com>
2020-11-21 14:47:47 -0300

    Import Model and import house model

---
+++ App/ImporterInterface/Parser/HouseImportModel.cs
@@ -0,0 +1,19 @@
+using System.Collections.Generic;
+
+namespace ImporterInterface
+{
+    public class HouseImportModel
+    {
+        public bool Avaible {get ; set; }
+        public int PricePerNight {get; set;}
+        public int TouristPointId {get ; set; }
+        public string Name {get ; set; }
+        public int Starts {get ; set; }
+        public string Address {get ; set; }
+        public List<string> Images {get ; set; }
+        public string Description {get ; set;}
+        public int Phone {get; set; }
+        public string Contact {get; set;}
+        
+    }
+}
