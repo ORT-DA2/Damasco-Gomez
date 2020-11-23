761c01a7c44da2cf50fef3742197e8133482c00e
yuligomez <yulianagomezsilva@gmail.com>
2020-11-22 21:11:53 -0300

    adding model in

---
+++ Front/UruguayNatrualWeb/src/app/models/house/house-model-in.ts
@@ -0,0 +1,20 @@
+
+
+export interface HouseModelIn {
+  id : number,
+  avaible : boolean,
+  pricePerNight : number,
+  touristPointId : number,
+  name : string,
+  starts : number,
+  address : string,
+  images : string [],
+  description : string,
+  phone : number,
+  contact: string,
+}
+
+
+
+
+
